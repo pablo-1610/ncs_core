a3cfffaa99c576c34ffaea6e78c4bca178d30256
PABLO-1610 <pablo.zapata.dev@gmail.com>
2022-05-27 01:10:03 +0200

    :art: Remove useless brackets

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,3 @@
---[[
-    This is NextCitizen's manifest.
-    This framework are developped by Pablo's Community.
-    You can help us to improve it. (discord.gg/g57xEz6J8A)
-]]
-
 fx_version 'cerulean';
 games { 'gta5' }
 lua54 'yes';
@@ -13,17 +7,14 @@ author 'Pablo1610 & his Community';
 description 'NextCitizen, an powerfull FiveM framework RolePlay-Based';
 version '1.0.0';
 
-shared_scripts
-{
+shared_scripts {
 
 };
 
-server_scripts
-{
+server_scripts {
 
 };
 
-client_scripts
-{
+client_scripts {
 
 };
