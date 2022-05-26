3d03199c44f6b04e74accab27091540c53844fbe
PABLO-1610 <pablo.zapata.dev@gmail.com>
2022-05-27 01:56:24 +0200

    :wrench: Populate fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,13 +7,21 @@ description 'NextCitizen, an powerfull FiveM framework RolePlay-Based';
 version '1.0.0-alpha';
 
 shared_scripts {
-
+    "src/core/shared.lua",
+    "src/enum/*.lua",
+    "src/class/**/shared.lua",
 };
 
 server_scripts {
-
+    "src/core/server.lua",
+    "src/class/**/server.lua",
 };
 
 client_scripts {
+    "src/core/shard.lua",
+    "src/class/**/client.lua",
+};
 
+shared_scripts {
+    "src/core/export.lua",
 };
