04afe6dc2a4404197128bcecf8606aff02621142
v4mmpp <zebientmax007@gmail.com>
2022-05-26 12:03:34 +0400

    added manifest file to project

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+--[[
+    This is NextCitizen's manifest.
+    This framework are developped by Pablo's Community.
+    You can help us to improve it. (discord.gg/g57xEz6J8A)
+]]
+
+fx_version 'cerulean';
+games { 'gta5' }
+lua54 'yes';
+use_fxv2_oal 'yes';
+
+author 'Pablo1610 & his Community';
+description 'NextCitizen, an powerfull FiveM framework RolePlay-Based';
+version '1.0.0';
+
+shared_scripts
+{
+
+};
+
+server_scripts
+{
+
+};
+
+client_scripts
+{
+
+};
