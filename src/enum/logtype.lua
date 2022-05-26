bf153f33601b64476323ba91240c61cbef8b82d1
PABLO-1610 <pablo.zapata.dev@gmail.com>
2022-05-27 01:57:23 +0200

    :label: Add LogType enum

---
+++ src/enum/logtype.lua
@@ -0,0 +1,7 @@
+_NCSEnum.LogType = {
+    DEBUG = {displayName = "DEBUG", displayColor = "^0"},
+    SUCCESS = {displayName = "SUCCESS", displayColor = "^2"},
+    INFO = {displayName = "INFO", displayColor = "^4"},
+    WARNING = {displayName = "WARNING", displayColor = "^3"},
+    ERROR = {displayName = "ERROR", displayColor = "^1"},
+}
