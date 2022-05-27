6ea99b8f2beffb9df994eba2ebb7b5a0ff900444
Florian <68273015+Florian1801@users.noreply.github.com>
2022-05-27 11:40:55 +0200

    add player keyboard input

---
+++ src/api/players/client/keyboardInput.lua
@@ -0,0 +1,25 @@
+---keyboardInput
+---@param TextEntry string
+---@param ExampleText string
+---@param MaxStringLenght number
+function _NCS:keyboardInput(TextEntry, ExampleText, MaxStringLenght)
+
+    AddTextEntry('FMMC_KEY_TIP1', TextEntry)
+
+    blockinput = true
+    DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "Somme", ExampleText, "", "", "", MaxStringLenght)
+    while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
+        Wait(0)
+    end
+
+    if UpdateOnscreenKeyboard() ~= 2 then
+        local result = GetOnscreenKeyboardResult()
+        Wait(500)
+        blockinput = false
+        return result
+    else
+        Citizen.Wait(500)
+        blockinput = false
+        return nil
+    end
+end
