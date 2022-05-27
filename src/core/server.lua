06508107f2a3801f97611bc7a268f8237ff0c2af
PABLO-1610 <pablo.zapata.dev@gmail.com>
2022-05-27 19:47:39 +0200

    :truck: Move startup handlers to onResourceStart-core

--- src/core/server.lua
+++ src/core/server.lua
@@ -6,6 +6,3 @@
 function _NCS:triggerClientEvent(eventName, targetId, ...)
     TriggerClientEvent(self:formatEvent(eventName), targetId, ...)
 end
-
-_NCS:trace("Loading NCS Core, please wait...", _NCSEnum.LogType.INFO)
-_NCS:triggerEvent("ncs_core:loaded")
