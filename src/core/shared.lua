f173ff618e253c2daaf68a11f858f616d876a299
PABLO-1610 <pablo.zapata.dev@gmail.com>
2022-05-27 02:26:19 +0200

    :hammer: Add event for _NCS#trace

--- src/core/shared.lua
+++ src/core/shared.lua
@@ -14,7 +14,7 @@ end
 ---@param logType any
 ---@public
 function _NCS:trace(message, logType)
-    logType = logType or _NCSEnum.LogType.DEBUG
+    logType = _NCSEnum._getLogTypeDisplayData(logType or _NCSEnum.LogType.DEBUG)
     print(("[%s^7] %s"):format(("%s%s"):format(logType.displayColor, logType.displayName), message))
 end
 
@@ -25,6 +25,10 @@ end
 function _NCS:formatEvent(eventName)
     return (("ncs:%s"):format(GetHashKey(eventName)))
 end
+
+AddEventHandler("ncs_core:trace", function(message, logType)
+    _NCS:trace(message, logType)
+end)
 
 _G._NCS = _NCS
 _G._NCSEnum = {}
