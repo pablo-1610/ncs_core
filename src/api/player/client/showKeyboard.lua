abd451da5fd1ffcc88a01f03fd50a7ced357cd63
Shazuub <54162106+Shazuub@users.noreply.github.com>
2022-05-27 20:52:39 +0200

    Update showKeyboard.lua

--- src/api/player/client/showKeyboard.lua
+++ src/api/player/client/showKeyboard.lua
@@ -4,18 +4,15 @@
 function API_Player:showKeyboard(textEntry, maxInputLength)
     AddTextEntry('FMMC_KEY_TIP1', TextEntry)
     DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "", '', "", "", "", maxInputLength)
-    blockinput = true
     while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
         Wait(0)
     end
     if UpdateOnscreenKeyboard() ~= 2 then
         local result = GetOnscreenKeyboardResult()
         Wait(500)
-        blockinput = false
         return result
     else
         Wait(500)
-        blockinput = false
         return nil
     end
 end 
