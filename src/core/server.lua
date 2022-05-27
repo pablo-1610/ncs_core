c35c3ae60a57773a25521598a22957f6ac972593
Walker <80550093+Walker974@users.noreply.github.com>
2022-05-27 09:41:06 +0100

    Add _NCS#triggerClientEvent

--- src/core/server.lua
+++ src/core/server.lua
@@ -1,1 +1,9 @@
 _NCS:trace("Loading NCS Core, please wait...")
+
+---triggerClientEvent
+---@param eventName string
+---@return void
+---@public
+function _NCS:triggerClientEvent(eventName, ...)
+    TriggerClientEvent(self:formatEvent(eventName), ...)
+end
