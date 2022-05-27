7c35ec473e9f3a774eae5717e58357a004c6d827
MoskalykA <streamarthur462@gmail.com>
2022-05-27 21:09:35 +0200

    🐛Fix the following error: attempt to yield from outside a coroutine.

--- src/api/player/client/showKeyboard.lua
+++ src/api/player/client/showKeyboard.lua
@@ -3,17 +3,19 @@
 ---@param exampleText string
 ---@param maxInputLength number
 function API_Player:showKeyboard(textEntry, exampleText, maxInputLength)
-    AddTextEntry("FMMC_KEY_TIP1", textEntry)
-    DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "Somme", exampleText, "", "", "", maxInputLength)
-    while (UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2) do
-        Wait(0)
-    end
-    if (UpdateOnscreenKeyboard() ~= 2) then
-        local result <const> = GetOnscreenKeyboardResult()
-        Wait(500)
-        return (result)
-    else
-        Citizen.Wait(500)
-        return (nil)
-    end
+    CreateThread(function()
+        AddTextEntry("FMMC_KEY_TIP1", textEntry)
+        DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "Somme", exampleText, "", "", "", maxInputLength)
+        while (UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2) do
+            Wait(0)
+        end
+        if (UpdateOnscreenKeyboard() ~= 2) then
+            local result <const> = GetOnscreenKeyboardResult()
+            Wait(500)
+            return (result)
+        else
+            Citizen.Wait(500)
+            return (nil)
+        end
+    end)
 end
