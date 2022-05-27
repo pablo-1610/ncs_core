60cd7c9acf1d20261e85fcc965c9a233e35cdf4d
v4mpp <101105433+v4mmpp@users.noreply.github.com>
2022-05-27 14:03:43 +0400

    Update groupDigits.lua

--- src/api/math/groupDigits.lua
+++ src/api/math/groupDigits.lua
@@ -4,5 +4,5 @@
 ---@public
 function _NCS:groupDigits(digits)
     local left, num, right = string.match(digits,'^([^%d]*%d)(%d*)(.-)$')
-	return tostring(left .. (num:reverse():gsub('(%d%d%d)','%1' .. ' '):reverse()) ..right );
+    return (tostring(left .. (num:reverse():gsub('(%d%d%d)','%1' .. ' '):reverse()) ..right));
 end
