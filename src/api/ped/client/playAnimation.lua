ed0e1e5b9ece876aad50741fa5bdb8b4f53234f0
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2022-05-27 21:53:05 +0200

    🐛Fix the following error: attempt to yield from outside a coroutine.

--- src/api/ped/client/playAnimation.lua
+++ src/api/ped/client/playAnimation.lua
@@ -9,19 +9,21 @@
 ---@return nil
 ---@public
 function API_Ped:playAnimation(ped, dict, anim, flag, blendin, blendout, playbackRate, duration)
-    if (not (DoesEntityExist(ped))) then
-        _NCS:die("Target ped does not exists")
-    end
-
-    blendin = blendin or 1.0
-    blendout = blendout or 1.0
-    playbackRate = playbackRate or 1.0
-    duration = duration or -1
-
-    RequestAnimDict(dict)
-    while (not (HasAnimDictLoaded(dict))) do
-        Wait(1)
-    end
-    TaskPlayAnim(ped, dict, anim, blendin, blendout, duration, flag, playbackRate, 0, 0, 0)
-    RemoveAnimDict(dict)
+    CreateThread(function()
+        if (not (DoesEntityExist(ped))) then
+            return _NCS:die("Target ped does not exists")
+        end
+    
+        blendin = blendin or 1.0
+        blendout = blendout or 1.0
+        playbackRate = playbackRate or 1.0
+        duration = duration or -1
+    
+        RequestAnimDict(dict)
+        while (not (HasAnimDictLoaded(dict))) do
+            Wait(1)
+        end
+        TaskPlayAnim(ped, dict, anim, blendin, blendout, duration, flag, playbackRate, 0, 0, 0)
+        RemoveAnimDict(dict)
+    end)
 end
