5e5a5fa69587d85332bbba4d348674b0a7150cd4
Wz <68116801+Wz-FiveM@users.noreply.github.com>
2022-05-27 10:48:38 +0200

    🔧 Fix _NCS#deleteVehicle

--- src/api/vehicles/client/deleteVehicle.lua
+++ src/api/vehicles/client/deleteVehicle.lua
@@ -1,6 +1,7 @@
----DeleteVehicle
--- @param vehicle Vehicle
--- @public
+---deleteVehicle
+---@param vehicle Vehicle
+---@return
+---@public
 function _NCS:deleteVehicle(vehicle)
     SetEntityAsMissionEntity(vehicle, 0, 1)
     DeleteVehicle(vehicle)
