6a95f11f9a6d3b8eac02a2dd73313016aca1b520
PABLO-1610 <pablo.zapata.dev@gmail.com>
2022-05-27 02:27:38 +0200

    :building_construction: Change LogType enum struct

--- src/enum/logtype.lua
+++ src/enum/logtype.lua
@@ -1,7 +1,12 @@
 _NCSEnum.LogType = {
-    DEBUG = {displayName = "DEBUG", displayColor = "^0"},
-    SUCCESS = {displayName = "SUCCESS", displayColor = "^2"},
-    INFO = {displayName = "INFO", displayColor = "^4"},
-    WARNING = {displayName = "WARNING", displayColor = "^3"},
-    ERROR = {displayName = "ERROR", displayColor = "^1"},
+    DEBUG = 1,
+    SUCCESS = 2,
+    INFO = 3,
+    WARNING = 4,
+    ERROR = 5,
 }
+
+_NCSEnum._getLogTypeDisplayData = function(logType)
+    local displayByType = { [1] = { displayName = "DEBUG", displayColor = "^0" }, [2] = { displayName = "SUCCESS", displayColor = "^2" }, [3] = { displayName = "INFO", displayColor = "^4" }, [4] = { displayName = "WARNING", displayColor = "^3" }, [5] = { displayName = "ERROR", displayColor = "^1" } }
+    return displayByType[logType]
+end
