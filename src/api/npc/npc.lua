65509e70399964e0d54f049db8f39812f276cb48
Walker <80550093+Walker974@users.noreply.github.com>
2022-05-27 11:37:15 +0100

    ✨ Create npc

---
+++ src/api/npc/npc.lua
@@ -0,0 +1,15 @@
+---npc
+---@param ped_type number
+---@param modelhash string
+---@param coords table
+---@param heading number
+---@public
+
+function _NCS:createped(ped_type, modelhash, coords, heading)
+    self.pedtype = ped_type
+    self.modelhash = modelhash
+    self.coords = coords
+    self.heading = heading
+    self.ped = CreatePed(self.pedtype, self.modelhash, self.coords.x, self.coords.y, self.coords.z, self.heading, false, true)
+end
+
