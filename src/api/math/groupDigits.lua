1add65be4692d92f4c8ed34cb4f8a35a4884ed4a
v4mmpp <zebientmax007@gmail.com>
2022-05-27 13:46:54 +0400

    :sparkles: Added _NCS#groupDigits

---
+++ src/api/math/groupDigits.lua
@@ -0,0 +1,8 @@
+---groupDigits
+---@param digits number
+---@return string
+---@public
+function _NCS:groupDigits(digits)
+    local left, num, right = string.match(digits,'^([^%d]*%d)(%d*)(.-)$')
+	return tostring(left .. (num:reverse():gsub('(%d%d%d)','%1' .. ' '):reverse()) ..right );
+end
