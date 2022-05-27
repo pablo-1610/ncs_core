8bf06eaca7b90a2c58c65885876789f631129d78
Florian <68273015+Florian-1801@users.noreply.github.com>
2022-05-27 11:50:40 +0200

    Update src/api/players/client/keyboardInput.lua
    Co-authored-by: Pablo Zapata <pablo.zapata.dev@gmail.com>

--- src/api/players/client/keyboardInput.lua
+++ src/api/players/client/keyboardInput.lua
@@ -8,7 +8,7 @@ function _NCS:keyboardInput(TextEntry, ExampleText, MaxStringLenght)
 
     blockinput = true
     DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "Somme", ExampleText, "", "", "", MaxStringLenght)
-    while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
+    while (UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2) do
         Wait(0)
     end
 
