30e6235bb0c5952c9904b3fc0bd402584049e8d2
Florian <68273015+Florian1801@users.noreply.github.com>
2022-05-27 20:40:37 +0200

    🔨 Add can use registered key

---
+++ src/api/player/client/canUseRegisteredKey.lua
@@ -0,0 +1,16 @@
+---canUseRegisterKey
+---@param bool boolean
+---@param buttonIndex string
+function API_Player:canUseRegisteredKey(bool, buttonIndex)
+    if (type(bool) == 'boolean') then
+        for _,v in pairs(registeredKeysTable) do
+            if (buttonIndex) then
+                if (v.key == string.upper(buttonIndex)) then
+                    v.canUse = Bool
+                end
+            else
+                v.canUse = Bool
+            end
+        end
+    end
+end
