97f7dafc67887b7b897fcc078b60dac05b774326
Florian <68273015+Florian-1801@users.noreply.github.com>
2022-05-27 15:38:36 +0200

    Update keyboardInput.lua

--- src/api/players/client/keyboardInput.lua
+++ src/api/players/client/keyboardInput.lua
@@ -1,12 +1,12 @@
 ---keyboardInput
----@param TextEntry string
----@param ExampleText string
----@param MaxStringLenght number
-function _NCS:keyboardInput(TextEntry, ExampleText, MaxStringLenght)
+---@param textEntry string
+---@param exampleText string
+---@param maxStringLenght number
+function _NCS:keyboardInput(textEntry, exampleText, maxStringLenght)
 
-    AddTextEntry('FMMC_KEY_TIP1', TextEntry)
+    AddTextEntry('FMMC_KEY_TIP1', textEntry)
 
-    DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "", ExampleText, "", "", "", MaxStringLenght)
+    DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "", exampleText, "", "", "", maxStringLenght)
     while (UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2) do
         Wait(0)
     end
