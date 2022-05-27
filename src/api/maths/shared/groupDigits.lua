4b549a76dae3e730df45d5ace270b999b987122d
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2022-05-27 19:48:17 +0200

    🎨 Removal of semicolons.

--- src/api/maths/shared/groupDigits.lua
+++ src/api/maths/shared/groupDigits.lua
@@ -4,5 +4,5 @@
 ---@public
 function API_Maths:groupDigits(digits)
     local left, num, right = string.match(digits, '^([^%d]*%d)(%d*)(.-)$')
-    return (tostring(left .. (num:reverse():gsub('(%d%d%d)', '%1' .. ' '):reverse()) .. right));
+    return (tostring(left .. (num:reverse():gsub('(%d%d%d)', '%1' .. ' '):reverse()) .. right))
 end
