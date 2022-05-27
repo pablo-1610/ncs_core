345942b936181522b3f17cd0beeee5a0ab873478
Florian <68273015+Florian1801@users.noreply.github.com>
2022-05-27 20:39:55 +0200

    🔨 Add on control press

---
+++ src/api/player/client/onControlPress.lua
@@ -0,0 +1,19 @@
+---onControlPress
+---@param key string
+---@param action function
+function API_Player:onControlPress(key, action)
+    if IsControlPressed(1, key) then
+        if (IsControlJustReleased(1, key)) then
+            if (action) then
+                action()
+            else
+                _NCS:trace("no action defined", 4)
+            end
+            return true
+        end
+    else
+        _NCS:trace("key not defind", 4)
+    end
+end
+
+
