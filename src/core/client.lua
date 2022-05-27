ad80f548d223ea718776b0db50e93e70795b5a81
PABLO-1610 <pablo.zapata.dev@gmail.com>
2022-05-27 17:34:05 +0200

    :building_construction: Big change in structure for net functions

--- src/core/client.lua
+++ src/core/client.lua
@@ -1,11 +1,3 @@
----triggerServerEvent
----@param eventName string
----@return void
----@public
-function _NCS:triggerServerEvent(eventName, ...)
-    TriggerServerEvent(self:formatEvent(eventName), ...)
-end
-
 CreateThread(function()
     _NCS:triggerEvent("ncs_core:loaded")
 end)
