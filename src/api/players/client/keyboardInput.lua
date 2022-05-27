091a4dfe7e66f176f8efdd9ed9e36ef7ba506982
Florian <68273015+Florian1801@users.noreply.github.com>
2022-05-27 14:05:45 +0200

    🔨 Update keyboard input

--- src/api/players/client/keyboardInput.lua
+++ src/api/players/client/keyboardInput.lua
@@ -6,20 +6,16 @@ function _NCS:keyboardInput(TextEntry, ExampleText, MaxStringLenght)
 
     AddTextEntry('FMMC_KEY_TIP1', TextEntry)
 
-    blockinput = true
-    DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "Somme", ExampleText, "", "", "", MaxStringLenght)
+    DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "", ExampleText, "", "", "", MaxStringLenght)
     while (UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2) do
         Wait(0)
     end
 
     if (UpdateOnscreenKeyboard() ~= 2) then
-        local result = GetOnscreenKeyboardResult()
         Wait(500)
-        blockinput = false
-        return (result)
+        return (GetOnscreenKeyboardResult())
     else
         Citizen.Wait(500)
-        blockinput = false
         return (nil)
     end
 end
