dfd3306a64775123ae05f92409f71533100a2941
ZAKIO <66121290+ZAKIO59@users.noreply.github.com>
2022-05-28 21:08:32 +0200

    :hammer: Get hash for API_Objects:serverCreateObject
    In order to be coordinated with other functions

--- src/api/objects/server/serverCreateObject.lua
+++ src/api/objects/server/serverCreateObject.lua
@@ -7,7 +7,7 @@
 ---@return void
 ---@public
 function API_Objects:serverCreateObject(props, x, y, z, h)
-    local object = CreateObject(props, x, y, z - 1, true)
+    local object = CreateObject(GetHashKey(props), x, y, z - 1, true)
     SetEntityHeading(object, h)
     FreezeEntityPosition(object, true)
 end
