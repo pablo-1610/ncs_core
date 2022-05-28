279e9480115170d584db51f47dc3fe4b0bdc2302
Zeleeer <106283602+Zeleeer@users.noreply.github.com>
2022-05-28 18:11:55 +0200

    :hammer: Fix

--- src/api/scaleform/client/requestMovie.lua
+++ src/api/scaleform/client/requestMovie.lua
@@ -3,11 +3,9 @@
 ---@return scaleform
 ---@public
 function API_Scaleform:requestMovie(movie)
-
     local scaleform = RequestScaleformMovie(movie)
-
     while not HasScaleformMovieLoaded(scaleform) do
         Wait(0)
     end
-    return scaleform
+    return (scaleform)
 end
