e931d7f9f3a8a022f8f52293290bafc5f00ea0f7
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2022-05-28 09:44:12 +0200

    🎨 Re-structuring the code.

--- src/api/discord/server/webhook.lua
+++ src/api/discord/server/webhook.lua
@@ -1,24 +1,14 @@
 ---webhook
 ---@param webhookLink string
----@param webhookName string
----@param webhookAvatar string
----@param content string
----@param embeds Array
+---@param webhookOptions Array
 ---@return void
 ---@public
-function API_Discord:webhook(webhookLink, webhookName, webhookAvatar, content, embeds)
+function API_Discord:webhook(webhookLink, webhookOptions)
    PerformHttpRequest(
       webhookLink, 
       function(err, text, headers) end, 
       "POST", 
-      json.encode(
-         {
-            username = webhookName, 
-            avatar_url = webhookAvatar,
-            content = content,
-            embeds = embeds
-         }
-      ), 
+      json.encode(webhookOptions), 
       { 
          ["Content-Type"] = "application/json" 
       }
