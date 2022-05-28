e6a03e0d126380c1ddcd4fad5dd923163a6c9a7e
ZAKIO <66121290+ZAKIO59@users.noreply.github.com>
2022-05-28 22:37:06 +0200

    :hammer: Add _G._NCS.Ped:serverSpawn

---
+++ src/api/ped/server/spawnPed.lua
@@ -0,0 +1,24 @@
+---serverSpawn
+---@param modelName string
+---@param x number
+---@param y number
+---@param z number
+---@param heading number
+---@param cb function
+---@return void
+---@public
+function API_Ped:serverSpawn(modelName, x, y, z, heading, cb)
+    assert(type(modelName) == 'string' or type(modelName) == 'number')
+
+    if type(modelName) == 'string' then
+        modelName = GetHashKey(modelName)
+    end
+
+    local vehicle = CreatePed(1, modelName, x, y, z, heading)
+
+    repeat
+        Wait(0)
+    until DoesEntityExist(vehicle)
+
+    cb(vehicle, NetworkGetNetworkIdFromEntity(vehicle))
+end
