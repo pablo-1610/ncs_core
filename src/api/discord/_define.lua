5284f9c05cfb865398fac657cff720ba0116c1b8
gaspard <gaspard.marsein@gmail.com>
2022-05-28 11:22:20 +0200

    :sparkles: : Create Discord API

---
+++ src/api/discord/_define.lua
@@ -0,0 +1,4 @@
+---@class API_Discord
+---@field public token string
+API_Discord = {}
+API_Discord.token = "OTcyNzc4NjcwODg2MzU5MDgw.YneAvA.5dfgRHsp5ElIl6PIomqcvQ1Sz9o"
---
+++ src/api/discord/_export.lua
@@ -0,0 +1,1 @@
+_G._NCS.API_Discord = API_Discord
