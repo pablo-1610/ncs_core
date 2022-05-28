bcb1b4296a0fe69c1fce4bb68843a3394937f298
Wz <68116801+Wz-FiveM@users.noreply.github.com>
2022-05-28 10:36:27 +0200

    🩹 Fix showKeyboard

--- src/api/player/client/showKeyboard.lua
+++ src/api/player/client/showKeyboard.lua
@@ -7,7 +7,7 @@ function API_Player:showKeyboard(textEntry, exampleText, maxInputLength)
     AddTextEntry('FMMC_KEY_TIP1', textEntry)
     DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "", exampleText, "", "", "", maxInputLength)
 
-    while (UpdateOnscreenKeyboard() ~= 1) and (UpdateOnscreenKeyboard() ~= 2) do
+    while (UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2) do
         Citizen.Wait(0)
     end
 
