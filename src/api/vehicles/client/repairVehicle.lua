f4e43774f8331555dfb75d41afcb20df135a60c2
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-05-28 11:46:07 +0200

    :hammer: Update API_Vehicles#repair

--- src/api/vehicles/client/repairVehicle.lua
+++ src/api/vehicles/client/repairVehicle.lua
@@ -1,11 +1,9 @@
 ---repair
 ---@param vehicleEntity number
----@param repairDeformation boolean
 ---@return void
 ---@public
 function API_Vehicles:repair(vehicleEntity)
     SetVehicleFixed(vehicleEntity)
     SetVehicleDirtLevel(vehicleEntity, 0.0)
- 
     SetVehicleDeformationFixed(vehicleEntity)
  end
