84995cb2cb7a50699f42b8bf89e80615ef1e08ee
MoskalykA <streamarthur462@gmail.com>
2022-05-28 14:56:10 +0200

    ✅ A small test.

--- src-web/src/App.tsx
+++ src-web/src/App.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react'
+import receiveNuiEvent from './providers/receiveNuiEvent'
+
 function App() {
+  const [name, setName] = useState('')
+  receiveNuiEvent('core', 'set_menu_data', (data: {
+    name: string
+  }) => {
+    setName(data.name)
+  })
+
   return (
-    <>
-      <h1 className="text-red-700">Hi!</h1>
-    </>
+    <div className="bg-zinc-900 border border-gray-500 w-fit mt-2 ml-2 px-2 py-1 rounded">
+      <h1 className="text-white font-mono text-xl">{name ? `Hi ${name}!` : 'Hi!'}</h1>
+    </div>
   )
 }
 
--- src-web/src/main.tsx
+++ src-web/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/index.css'
+import Visibility from './providers/visibility'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <>
+      <Visibility moduleName="core" visibleComponent={<App />} />
+    </>
   </React.StrictMode>
 )
