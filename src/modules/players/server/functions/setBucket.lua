b3f7a38af2c78c43d4ac64930227aa8cdb7301c1
ach <82474261+ach-git@users.noreply.github.com>
2022-05-28 17:42:30 +0200

    Update setBucket.lua

--- src/modules/players/server/functions/setBucket.lua
+++ src/modules/players/server/functions/setBucket.lua
@@ -1,6 +1,8 @@
 ---setBucket
 ---@param playerID number
 ---@param bucketID number
+---@public
+---@return void
 function MOD_Players:setBucket(playerID, bucketID)
     SetEntityRoutingBucket(playerID, bucketID)
 end
