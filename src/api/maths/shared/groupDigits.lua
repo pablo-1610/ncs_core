86e73207a5032d22920ebde63b10ddfb7a06ff31
MoskalykA <streamarthur462@gmail.com>
2022-05-29 00:13:02 +0200

    🎨 Restructuring the code to avoid some bugs.

--- src/api/maths/shared/groupDigits.lua
+++ src/api/maths/shared/groupDigits.lua
@@ -3,6 +3,6 @@
 ---@return string
 ---@public
 function API_Maths:groupDigits(digits)
-    local left, num, right = string.match(digits, '^([^%d]*%d)(%d*)(.-)$')
-    return (tostring(left .. (num:reverse():gsub('(%d%d%d)', '%1' .. ' '):reverse()) .. right))
+    local left, num, right = string.match(digits, "^([^%d]*%d)(%d*)(.-)$")
+    return (tostring(left .. (num:reverse():gsub("(%d%d%d)", "%1 "):reverse()) .. right))
 end
