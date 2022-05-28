7fdc18a52a9653c83bfc988530e0af64f355951b
Zeleeer <106283602+Zeleeer@users.noreply.github.com>
2022-05-28 18:13:22 +0200

    :hammer: Fix

--- src/api/scaleform/client/showBigFreemodeMessage.lua
+++ src/api/scaleform/client/showBigFreemodeMessage.lua
@@ -5,19 +5,15 @@
 ---@return void
 ---@public
 function API_Scaleform:showBigFreemodeMessage(title, message, sec)
-
     local scaleform = API_Scaleform:requestMovie("MP_BIG_MESSAGE_FREEMODE")
-
-    BeginScaleformMovieMethod(scaleform, 'SHOW_SHARD_WASTED_MP_MESSAGE')
+    BeginScaleformMovieMethod(scaleform, "SHOW_SHARD_WASTED_MP_MESSAGE")
     ScaleformMovieMethodAddParamTextureNameString(title)
     ScaleformMovieMethodAddParamTextureNameString(message)
     EndScaleformMovieMethod()
-
     while sec > 0 do
         Citizen.Wait(1)
         sec = sec - 0.01
         DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
     end
-
     SetScaleformMovieAsNoLongerNeeded(scaleform)
 end
