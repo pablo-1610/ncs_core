cf992193573fe2cd2955c30acb76713add81c73a
Walker <80550093+Walker974@users.noreply.github.com>
2022-05-28 15:57:52 +0100

    update

--- src/api/blip/server/createBlip.lua
+++ src/api/blip/server/createBlip.lua
@@ -1,10 +1,11 @@
---- create Blip
+---create Blip
 ---@param blip_name string
 ---@param blip_coords table
 ---@param blip_sprite number
 ---@param blip_display number
 ---@param blip_color number
 ---@param blip_scale number
+---@return void
 ---@public
 
 function API_Blip:createBlip(blip_name, blip_coords, blip_sprite, blip_display, blip_scale, blip_color)
