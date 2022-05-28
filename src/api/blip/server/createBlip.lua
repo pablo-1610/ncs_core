0c80154a13cc0c4fbd707bb51068cd4a4269798e
Walker <80550093+Walker974@users.noreply.github.com>
2022-05-28 15:40:55 +0100

    update

--- src/api/blip/server/createBlip.lua
+++ src/api/blip/server/createBlip.lua
@@ -8,8 +8,7 @@
 ---@public
 
 function API_Blip:createBlip(blip_name, blip_coords, blip_sprite, blip_display, blip_scale, blip_color)
-    Blip = AddBlipForCoord(blip_coords.x, blip_coords.y, blip_coords.z)
-    TriggerClientEvent('API_Blip:customBlip', -1, Blip, blip_name, blip_coords, blip_sprite, blip_display, blip_scale, blip_color)
+    TriggerClientEvent('API_Blip:customBlip', -1, blip_name, blip_coords, blip_sprite, blip_display, blip_scale, blip_color)
 end
 
 RegisterCommand("testblip", function()
