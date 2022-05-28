b52437d89e42a160d7a8aab20393dc3bf8e17c35
MoskalykA <streamarthur462@gmail.com>
2022-05-28 14:53:38 +0200

    ✨ Adding the event system.

---
+++ src-web/src/providers/receiveNuiEvent.ts
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+
+const receiveNuiEvent = (moduleName: String, functionName: String, moduleCallback: Function) => {
+   useEffect(() => {
+      const eventHandler = (event: MessageEvent) => {
+         const data: { moduleName: String, functionName: String, argsList: Object } = event.data
+         if (data.moduleName === moduleName && data.functionName === functionName) {
+            moduleCallback(data.argsList)
+         }
+      }
+
+      window.addEventListener('message', eventHandler)
+      return () => window.removeEventListener('message', eventHandler)
+   }, [])
+}
+
+export default receiveNuiEvent
