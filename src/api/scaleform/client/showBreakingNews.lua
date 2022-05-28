e1f876c26025145d49f5f890796f1c5741914cbd
Zeleeer <106283602+Zeleeer@users.noreply.github.com>
2022-05-28 18:14:07 +0200

    :hammer: Fix

--- src/api/scaleform/client/showBreakingNews.lua
+++ src/api/scaleform/client/showBreakingNews.lua
@@ -6,30 +6,24 @@
 ---@return void
 ---@public
 function API_Scaleform:showBreakingNews(title, message, bottom, sec)
-
-    local scaleform = API_Scaleform:requestMovie('BREAKING_NEWS')
-
-    BeginScaleformMovieMethod(scaleform, 'SET_TEXT')
+    local scaleform = API_Scaleform:requestMovie("BREAKING_NEWS")
+    BeginScaleformMovieMethod(scaleform, "SET_TEXT")
     ScaleformMovieMethodAddParamTextureNameString(message)
     ScaleformMovieMethodAddParamTextureNameString(bottom)
     EndScaleformMovieMethod()
-
-    BeginScaleformMovieMethod(scaleform, 'SET_SCROLL_TEXT')
+    BeginScaleformMovieMethod(scaleform, "SET_SCROLL_TEXT")
     ScaleformMovieMethodAddParamInt(0)
     ScaleformMovieMethodAddParamInt(0)
     ScaleformMovieMethodAddParamTextureNameString(title)
     EndScaleformMovieMethod()
-
-    BeginScaleformMovieMethod(scaleform, 'DISPLAY_SCROLL_TEXT')
+    BeginScaleformMovieMethod(scaleform, "DISPLAY_SCROLL_TEXT")
     ScaleformMovieMethodAddParamInt(0)
     ScaleformMovieMethodAddParamInt(0)
     EndScaleformMovieMethod()
-
     while sec > 0 do
         Wait(1)
         sec = sec - 0.01
         DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
     end
-
     SetScaleformMovieAsNoLongerNeeded(scaleform)
 end
