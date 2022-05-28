2b574856a4367e20e45716b694de45bbb49e6eab
Walker <80550093+Walker974@users.noreply.github.com>
2022-05-28 16:15:09 +0100

    update

--- src/api/blip/client/customBlip.lua
+++ src/api/blip/client/customBlip.lua
@@ -1,6 +1,5 @@
 _NCS:registerNetEvent('API_Blip:customBlip')
 _NCS:handleEvent('API_Blip:customBlip', function(blip_name, blip_coords, blip_sprite, blip_display, blip_scale, blip_color)
-    
     local Blip = AddBlipForCoord(blip_coords.x, blip_coords.y, blip_coords.z)
     SetBlipSprite(Blip, blip_sprite)
     SetBlipDisplay(Blip, blip_display)
