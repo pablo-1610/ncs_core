e32f8fc6dc4d248cb4f03f931b01e9f840ee29d8
ZAKIO <66121290+ZAKIO59@users.noreply.github.com>
2022-05-29 15:50:32 +0200

    :hammer: Remove CreateThread

--- src/api/vehicles/client/spawnVehicle.lua
+++ src/api/vehicles/client/spawnVehicle.lua
@@ -6,29 +6,27 @@
 ---@return void
 ---@public
 function API_Vehicles:spawn(modelName, coords, heading, callback, clientSide)
-    CreateThread(function()
-        assert(type(modelName) == "string" or type(modelName) == "number")
-        if type(modelName) == "string" then
-            modelName = GetHashKey(modelName)
-        end
+    assert(type(modelName) == "string" or type(modelName) == "number")
+    if type(modelName) == "string" then
+        modelName = GetHashKey(modelName)
+    end
 
-        local model = GetHashKey(modelName)
-        if (not (IsModelInCdimage(model))) then
-            return _NCS:die("Model not found: " .. modelName)
-        end
-        RequestModel(model)
-        while (not (HasModelLoaded(model))) do
-            Wait(1)
-        end
-        local vehicle = CreateVehicle(model, coords, heading, not (clientSide), 0)
-        SetVehicleDirtLevel(vehicle, 0.0)
-        SetEntityCoordsNoOffset(vehicle, coords.x, coords.y, coords.z + 0.5, 0.0, 0.0, 0.0)
-        SetVehicleOnGroundProperly(vehicle)
-        SetEntityHeading(vehicle, heading)
-        SetEntityAsMissionEntity(vehicle, 1, 1)
-        if cb ~= nil then
-            cb(vehicle)
-        end
-        SetModelAsNoLongerNeeded(vehicle)
-    end)
+    local model = GetHashKey(modelName)
+    if (not (IsModelInCdimage(model))) then
+        return _NCS:die("Model not found: " .. modelName)
+    end
+    RequestModel(model)
+    while (not (HasModelLoaded(model))) do
+        Wait(1)
+    end
+    local vehicle = CreateVehicle(model, coords, heading, not (clientSide), 0)
+    SetVehicleDirtLevel(vehicle, 0.0)
+    SetEntityCoordsNoOffset(vehicle, coords.x, coords.y, coords.z + 0.5, 0.0, 0.0, 0.0)
+    SetVehicleOnGroundProperly(vehicle)
+    SetEntityHeading(vehicle, heading)
+    SetEntityAsMissionEntity(vehicle, 1, 1)
+    if cb ~= nil then
+        cb(vehicle)
+    end
+    SetModelAsNoLongerNeeded(vehicle)
 end
