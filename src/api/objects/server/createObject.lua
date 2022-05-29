208a661b237d2c782734f37f10d760234bfb22f8
ZAKIO <66121290+ZAKIO59@users.noreply.github.com>
2022-05-29 16:50:11 +0200

    Update createObject.lua

--- src/api/objects/server/createObject.lua
+++ src/api/objects/server/createObject.lua
@@ -1,15 +1,15 @@
 ---createObject
----@param props string
+---@param prop string
 ---@param x number
 ---@param y number
 ---@param z number
 ---@param heading number
 ---@return void
 ---@public
-function API_Objects:createObject(props, x, y, z, heading)
-    assert(type(props) == "string" or type(props) == "number")
-    if type(props) == "string" then
-        props = GetHashKey(props)
+function API_Objects:createObject(prop, x, y, z, heading)
+    assert(type(prop) == "string" or type(prop) == "number")
+    if type(prop) == "string" then
+        props = GetHashKey(prop)
     end
     local object = CreateObject(props, x, y, z, true)
     SetEntityHeading(object, heading)
