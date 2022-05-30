05f15c5d600dd40db60d4529fd5f4b3516f54d27
k <lomyedoo@gmail.com>
2022-05-30 17:22:31 +0200

    --

---
+++ src/api/camera/client/setCameraParams.lua
@@ -0,0 +1,15 @@
+---setCameraParams
+---@param coords table
+---@param rotationX number
+---@param rotationY number
+---@param rotationZ number
+---@param fov number
+---@return void
+---@public
+function NCSCamera:setCameraParams(coords, rotationX, rotationY, rotationZ, fov)
+    if self.exist then
+        SetCamCoord(self.id, coords)
+        SetCamRot(self.id, rotationX, rotationY, rotationZ, 2)
+        SetCamFov(self.id, params.fov)
+    end
+end
