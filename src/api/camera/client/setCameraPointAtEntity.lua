35cd7232b28a80d0711d71b5d80a0a943a65be9d
кавказ <77354422+K4VK4Z@users.noreply.github.com>
2022-05-30 16:03:20 +0200

    Update src/api/camera/client/setCameraPointAtEntity.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/api/camera/client/setCameraPointAtEntity.lua
+++ src/api/camera/client/setCameraPointAtEntity.lua
@@ -1,16 +1,15 @@
 ---setCameraPointAtEntity
----@param Camera NCSCamera
----@param entity number
+---@param entityId number
 ---@param offsetX number
 ---@param offsetY number
 ---@param offsetZ number
 ---@return void
 ---@public
-function NCSCamera:setCameraPointAtEntity(entity, offsetX, offsetY, offsetZ)
+function NCSCamera:setCameraPointAtEntity(entityId, offsetX, offsetY, offsetZ)
     if self.exist then
-        if (not (DoesEntityExist(entity))) then
+        if (not (DoesEntityExist(entityId))) then
             return _NCS:die("Target ped does not exists")
         end    
-        PointCamAtEntity(self.id, entity, offsetX or .0, offsetY or .0, offsetZ or .0, true)
+        PointCamAtEntity(self.id, entityId, offsetX or .0, offsetY or .0, offsetZ or .0, true)
     end
 end
