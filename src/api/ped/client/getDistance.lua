355bf479cf960cdd1f54de8bbdf321e2dd4c2ed1
k <lomyedoo@gmail.com>
2022-05-31 01:38:57 +0200

    Create getDistance.lua

---
+++ src/api/ped/client/getDistance.lua
@@ -0,0 +1,9 @@
+---getDistance
+---@param pedIn number
+---@param coords table
+---@return distance
+---@public
+function API_Player:getDistance(pedId, coords)
+    local distance <const> =  #(API_Ped:getPosition(pedId) - coords)
+	return distance
+end
