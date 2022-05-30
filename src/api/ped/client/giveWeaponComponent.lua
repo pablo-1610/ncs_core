05d2cd601c8c77deb5b82555d278fe8e79c4b0ff
Zeleeer <106283602+Zeleeer@users.noreply.github.com>
2022-05-30 18:51:25 +0200

    :hammer: API_Ped Add give/has/remove WeaponComponent

--- src/api/ped/client/giveWeaponComponent.lua
+++ src/api/ped/client/giveWeaponComponent.lua
@@ -6,9 +6,7 @@
 ---@public
 function API_Ped:giveWeaponComponent(pedId, weaponName, componentName)
     local weaponHash = GetHashKey(weaponName)
-    if API_Ped:hasWeapon(pedId, weaponHash) then
-        if not API_Ped:hasWeaponComponent(pedId, weaponHash, componentName) then
-            GiveWeaponComponentToPed(pedId, weaponHash, componentName)
-        end
+    if API_Ped:hasWeapon(pedId, weaponHash) and not API_Ped:hasWeaponComponent(pedId, weaponHash, componentName) then
+        GiveWeaponComponentToPed(pedId, weaponHash, componentName)
     end
 end
