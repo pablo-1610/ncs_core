e237db35e727a0e8fef35b177a723d893fb6d685
Zeleeer <106283602+Zeleeer@users.noreply.github.com>
2022-05-30 12:35:09 +0200

    :hammer: Move in API_Ped

--- src/api/player/client/removeWeapon.lua
+++
@@ -1,8 +0,0 @@
----removeWeapon
----@param weaponName string
----@return void
----@public
-function API_Player:removeWeapon(weaponName)
-    local weaponHash = GetHashKey(weaponName)
-    RemoveWeaponFromPed(PlayerPedId(), weaponHash)
-end
