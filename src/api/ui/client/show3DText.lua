b18963b399f226aa14b60eecaa4dffb1e067256c
Tarkiom <69655558+Tarkiom@users.noreply.github.com>
2022-05-31 21:53:23 +0200

    Add to UI API

---
+++ src/api/ui/client/show3DText.lua
@@ -0,0 +1,24 @@
+---show3DText
+---@param x number
+---@param y number
+---@param z number
+---@param text string
+---@param font number
+---@param scale number
+---@return void
+---@public
+function API_UI:show3DText(x, y, z, text, font, scale)
+    local onScreen, x, y = World3dToScreen2d(x, y, z)
+    if not scale then scale = 1 end
+    if not font then font = 4 end
+    if (onScreen) then
+        SetTextScale(0.0 * scale, 0.55 * scale)
+        SetTextFont(font)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 215)
+        SetTextEntry("STRING")
+        SetTextCentre(true)
+        AddTextComponentString(text)
+        DrawText(x, y)
+    end 
+end
