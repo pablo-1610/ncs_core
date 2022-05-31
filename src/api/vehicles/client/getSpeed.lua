c4a01b5f104d986e38ad41fbe4a06cfa7a5486ae
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-05-31 23:37:20 +0200

    :art: Code convention

--- src/api/vehicles/client/getSpeed.lua
+++ src/api/vehicles/client/getSpeed.lua
@@ -5,7 +5,7 @@
 function API_Vehicles:getSpeed(vehicleId)
     if (vehicleId) then
         local speed <const> = (GetEntitySpeed(vehicleId) * 3.6) --Set speed to km/h
-        return speed
+        return (speed)
     end
     _NCS:trace("Unable to find vehicle", 1)
 end
