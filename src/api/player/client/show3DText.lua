a51be1665e0dbaa06dcb2563626304b60678b63b
Tarkiom <69655558+Tarkiom@users.noreply.github.com>
2022-05-31 20:47:28 +0200

    :sparkles: Just added the 3d text

--- src/api/player/client/show3DText.lua
+++ src/api/player/client/show3DText.lua
@@ -3,11 +3,23 @@
 ---@param y number
 ---@param z number
 ---@param text string
+---@param font number
+---@param scale number
 ---@return void
 ---@public
-function API_Player:show3DText(x, y, z, text)
+function API_Player:show3DText(x, y, z, text,font,scale)
     local onScreen, x, y = World3dToScreen2d(x, y, z)
+    if not scale then scale = 1 end
+    if not font then font = 4 end
+
     if (onScreen) then
-        -- TODO : Draw text
-    end
+        SetTextScale(0.0 * scale, 0.55 * scale)
+        SetTextFont(font)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 215)
+        SetTextEntry("STRING")
+        SetTextCentre(true)
+        AddTextComponentString(text)
+        DrawText(x, y)
+    end 
 end
