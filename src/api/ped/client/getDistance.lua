e77d6443250746436b318aebbc8ba96e12c66714
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-05-31 23:31:45 +0200

    :art: Wrong return type

--- src/api/ped/client/getDistance.lua
+++ src/api/ped/client/getDistance.lua
@@ -1,9 +1,9 @@
 ---getDistance
 ---@param pedId number
 ---@param coords table
----@return distance
+---@return number
 ---@public
 function API_Player:getDistance(pedId, coords)
-    local distance <const> =  #(API_Ped:getPosition(pedId) - coords)
-    return distance
+    local distance <const> = #(API_Ped:getPosition(pedId) - coords)
+    return (distance)
 end
