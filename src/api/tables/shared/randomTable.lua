7da281373d8461d84d82d6455335b5fdf380b0fc
Ekali <pcnlmmm@gmail.com>
2022-06-01 00:33:13 +0200

    Create randomTable.lua
    • Get a random value in a table

---
+++ src/api/tables/shared/randomTable.lua
@@ -0,0 +1,12 @@
+---randomTable
+---@param tableR table
+---@return value
+---@public
+
+function randomTable(tableR)
+    if not tableR[1] then return end
+    local randM = math.random(0, #tableR)
+    if not tableR[randM] then return end
+  
+    return tableR[randM]
+end
