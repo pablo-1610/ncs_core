821351760b5f256f3c31729093bf2da68e50628e
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-05-31 23:37:11 +0200

    :art: Code convention

--- src/api/vehicles/client/getFuelTank.lua
+++ src/api/vehicles/client/getFuelTank.lua
@@ -5,7 +5,7 @@
 function API_Vehicles:getFuelTank(vehicleId)
     if (vehicleId) then
         local fuel <const> = GetVehicleHandlingFloat(vehicleId, "CHandlingData", "fPetrolTankVolume")
-        return fuel
+        return (fuel)
     end
     _NCS:trace("Unable to find vehicle", 1)
 end
