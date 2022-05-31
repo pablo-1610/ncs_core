dcfcccd08f0e07d652d4bed1cf5971653e8b202e
кавказ <77354422+K4VK4Z@users.noreply.github.com>
2022-05-31 17:45:23 +0200

    Update src/api/ped/client/getDistance.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/api/ped/client/getDistance.lua
+++ src/api/ped/client/getDistance.lua
@@ -1,9 +1,9 @@
 ---getDistance
----@param pedIn number
+---@param pedId number
 ---@param coords table
 ---@return distance
 ---@public
 function API_Player:getDistance(pedId, coords)
     local distance <const> =  #(API_Ped:getPosition(pedId) - coords)
-	return distance
+    return distance
 end
