1e1e7032f8cb0ed95ced294382ef5eb97d14d819
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-06-01 19:20:05 +0200

    :hammer: Add MOD_Players:existsInDatabase

---
+++ src/modules/players/server/functions/existsInDatabase.lua
@@ -0,0 +1,11 @@
+---existsInDatabase
+---@param identifier string
+---@param callback function
+---@public
+function MOD_Players:existsInDatabase(identifier, callback)
+    API_Database:query("SELECT 1 FROM ncs_players WHERE player_identifier = @player_identifier", {
+        ["player_identifier"] = identifier
+    }, function(rows)
+        callback(#rows > 0)
+    end)
+end
