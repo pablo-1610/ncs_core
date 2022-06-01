5a0e23ab8604099ff9d5fb451de69ea4e3ebe365
L4 <83190245+LQuatre@users.noreply.github.com>
2022-06-01 20:12:00 +0200

    Update sortTable.lua

--- src/api/tables/shared/sortTable.lua
+++ src/api/tables/shared/sortTable.lua
@@ -1,7 +1,8 @@
--- @param tableT table
--- @param sortFunc function
--- @return table
--- @public
+---sortTable
+---@param tableT table
+---@param sortFunc function
+---@return table
+---@public
 function API_Tables:sortTable(tableT, sortFunc)
     table.sort(tableT, sortFunc)
     return tableT
