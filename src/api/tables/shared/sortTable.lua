e4c9a3a1427a91d9ac5f96515c15b7ed04c36f30
dedoxtv <unmecpasfort@gmail.com>
2022-06-01 20:06:26 +0200

    Revert "✨ Add some table function"
    This reverts commit 27fbfe9d69cf9f655929d05d938ad653b7f3a889.

--- src/api/tables/shared/sortTable.lua
+++
@@ -1,9 +0,0 @@
--- fonction qui trie une table
--- @param tableT table
--- @param sortFunc function
--- @return table
--- @public
-function API_Tables:sortTable(tableT, sortFunc)
-    table.sort(tableT, sortFunc)
-    return tableT
-end
