f748641b33c22b7616129c714063abd979a76d7c
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-06-01 19:18:29 +0200

    :hammer: Update NCSPlayer

--- src/class/Player/server/NCSPlayer.lua
+++ src/class/Player/server/NCSPlayer.lua
@@ -1,28 +1,39 @@
 ---@class NCSPlayer
-NCSPlayer = {}
-NCSPlayer.__index = NCSPlayer
+local NCSPlayer = {}
+
+local __instance = {
+    __index = NCSPlayer,
+
+    __eq = function(a, b)
+        return (a.id == b.id)
+    end,
+
+    __tostring = function(self)
+        return ("%s [%s]"):format(self.name, self.id)
+    end
+}
 
 setmetatable(NCSPlayer, {
     __call = function(_, id)
-        local self = setmetatable({}, NCSPlayer)
+        local self = setmetatable({}, __instance)
+
         self.id = id
         self.name = GetPlayerName(self.id)
         self.identifier = API_Player:getIdentifier(self.id)
         self.inGame = false
+
         return (self)
     end
 })
 
 ---triggerEvent
 ---@param eventName string
----@return void
 ---@public
 function NCSPlayer:triggerEvent(eventName, ...)
     TriggerClientEvent(eventName, self.id, ...)
 end
 
 ---ready
----@return void
 ---@public
 function NCSPlayer:ready()
     self.inGame = true
