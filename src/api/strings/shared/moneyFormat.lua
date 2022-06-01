e758fc126cbf31413f30930d3eb51703da070a0d
CsokiHUN <feketetibi71@gmail.com>
2022-06-01 14:04:32 +0200

    :hammer: Fix variable types

--- src/api/strings/shared/moneyFormat.lua
+++ src/api/strings/shared/moneyFormat.lua
@@ -4,6 +4,6 @@
 ---@return string
 ---@public
 function API_Strings:moneyFormat(amount, stepper)
-	local left, center, right = string.match(math.floor(amount), "^([^%d]*%d)(%d*)(.-)$")
+	local left <const>, center <const>, right <const> = string.match(math.floor(amount), "^([^%d]*%d)(%d*)(.-)$")
 	return (left .. string.reverse(string.gsub(string.reverse(center), "(%d%d%d)", "%1" .. (stepper or " "))) .. right)
 end
