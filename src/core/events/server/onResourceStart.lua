6dfea842bf0356f807bc8c71c685c20adef6aba1
Ekali <pcnlmmm@gmail.com>
2022-06-02 00:06:34 +0200

    Add an update verification ( not optimized maybe )

--- src/core/events/server/onResourceStart.lua
+++ src/core/events/server/onResourceStart.lua
@@ -3,6 +3,21 @@ AddEventHandler("onResourceStart", function(resourceName)
         return
     end
 
+    PerformHttpRequest("https://raw.githubusercontent.com/NextCitizens/ncs_core/main/fxmanifest.lua", function (_, resultData, _)
+        local currentVersion <const> = GetResourceMetadata(GetCurrentResourceName(), "version")
+        local lines = {}
+
+        for s in resultData:gmatch("[^\r\n]+") do
+            table.insert(lines, s)
+        end
+
+        local ver <const> = API_Strings:split(lines[6], "'")[2]
+
+        if (not (currentVersion == ver)) then
+            _NCS:coreTrace("NCS Core has not up to date ^3please update -> https://github.com/NextCitizens/ncs_core ^7!", _NCSEnum.LogType.ERROR)
+        end
+    end)
+
     _NCS:coreTrace("NCS Core has been ^2started ^7!", _NCSEnum.LogType.INFO)
     _NCS:triggerEvent("ncs_core:loaded")
 end)
