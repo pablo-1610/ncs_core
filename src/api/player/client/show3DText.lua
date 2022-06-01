2587f30fb87a3730ba9c5b3b5f47a23fedfbdedf
Csoki <36936216+CsokiHUN@users.noreply.github.com>
2022-06-01 19:13:50 +0200

    Update src/api/player/client/show3DText.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/api/player/client/show3DText.lua
+++ src/api/player/client/show3DText.lua
@@ -9,14 +9,12 @@
 ---@public
 function API_Player:show3DText(x, y, z, text, font, scale)
     local onScreen <const>, x <const>, y <const> = GetScreenCoordFromWorldCoord(x, y, z)
-    scale = scale or 1
-    font = font  or 4
     if (not onScreen) then
         return
     end
 
-    SetTextScale(0.0 * scale, 0.55 * scale)
-    SetTextFont(font)
+    SetTextScale(0.0 * (scale or 1), 0.55 * (scale or 1))
+    SetTextFont((font or 4))
     SetTextProportional(1)
     SetTextColour(255, 255, 255, 215)
     SetTextEntry("STRING")
