c48c21ed83fe6cf1101c794a94c3f5330588ffbd
Csoki <36936216+CsokiHUN@users.noreply.github.com>
2022-06-01 11:15:15 +0200

    Update src/api/player/server/getPlayerIdentifiers.lua
    Co-authored-by: Pablo Zapata <pablo.zapata.dev@gmail.com>

--- src/api/player/server/getPlayerIdentifiers.lua
+++ src/api/player/server/getPlayerIdentifiers.lua
@@ -5,7 +5,7 @@
 function API_Player:getIdentifiers(playerSrc)
 	local identifiers = {}
 	for _, v in pairs(GetPlayerIdentifiers(playerSrc)) do
-		local splitted = _NCS.Strings:split(v, ":")
+		local splitted <const> = _NCS.Strings:split(v, ":")
 
 		identifiers[splitted[1]] = v
 	end
