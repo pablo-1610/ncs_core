de5bc5655d1f4a76d51b5df8b68ad41f18c3d12d
Tayz <73202029+TayzDev@users.noreply.github.com>
2022-06-01 21:24:02 +0200

    🌟 ADD de translation
    🌟 ADD de translation

--- src/localization/locales/lang_de.lua
+++ src/localization/locales/lang_de.lua
@@ -1,3 +1,32 @@
 _Literals = {
-    ERROR_ALREADY_CONNECTED = "Sie sind bereits angemeldet.",
+    ERROR_ALREADY_CONNECTED = "Sie sind bereits verbunden.",
+    ERROR_SERVER_IDENTIFICATION_METHOD_BROKEN = "Die Methode zur Identifizierung des Servers ist fehlerhaft. Bitte wenden Sie sich an den Eigentümer des Servers.",
+    ERROR_SERVER_CHARACTER_CREATION_FAILED = "Charaktererstellung fehlgeschlagen. Bitte versuchen Sie es erneut oder wenden Sie sich an das Personal des Servers.",
+
+    CONNECTION_RETRIEVING_CHARACTERS = "Charaktere abrufen...",
+    CONNECTION_RETRIEVING_PLAYER_DATA = "Abrufen von Spielerdaten...",
+
+    CONNECTION_WELCOME_MESSAGE = "Willkommen bei %s",
+    CONNECTION_CHARACTER_REQUIRED = "Um bei %s spielen zu können, müssen Sie mindestens einen Charakter erstellen.",
+    CONNECTION_BUTTON_CREATE = "Erstelle einen neuen Charakter",
+
+    CONNECTION_CHARACTER_CREATION_TITLE = "Erstelle einen neuen Charakter",
+    CONNECTION_CHARACTER_CREATION_SEX = "Geschlecht",
+    CONNECTION_CHARACTER_CREATION_SEX_MALE = "Männlich",
+    CONNECTION_CHARACTER_CREATION_SEX_FEMALE = "Weiblich",
+    CONNECTION_CHARACTER_CREATION_FIRSTNAME = "Vorname",
+    CONNECTION_CHARACTER_CREATION_LASTNAME = "Nachname",
+    CONNECTION_CHARACTER_CREATION_DOB = "Geburtsdatum",
+    CONNECTION_CHARACTER_CREATION_HEIGHT = "Größe", CONNECTION_CHARACTER_CREATION_HEIGHT = "Größe",
+    CONNECTION_CHARACTER_CREATION_BUTTON_CREATE = "Erstellen",
+    CONNECTION_CHARACTER_CREATION_BUTTON_GOBACK = "Zurückgehen",
+
+    CONNECTION_CHARACTER_SELECT_DESC = "Sie müssen einen Charakter zum Spielen auswählen",
+    CONNECTION_CHARACTER_SELECT_FETCH_BUTTON = "Meine Charaktere",
+    CONNECTION_CHARACTER_SELECT_CREATE_BUTTON = "Einen neuen Charakter erstellen",
+
+    CONNECTION_CHARACTER_SELECTION_TITLE = "Wählen Sie ein Zeichen",
+    CONNECTION_CHARACTER_SELECTION_BUTTON = "Mit %s spielen",
+
+    CONNECTING_CREATING_CHARACTER = "Charakter erstellen...",
 }
