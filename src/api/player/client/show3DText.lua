35d1bf485e7ba4598b537f21059fd90864fda2a8
CsokiHUN <feketetibi71@gmail.com>
2022-06-01 14:02:55 +0200

    :hammer: Update show3DText() functions

--- src/api/player/client/show3DText.lua
+++ src/api/player/client/show3DText.lua
@@ -8,21 +8,26 @@
 ---@return void
 ---@public
 function API_Player:show3DText(x, y, z, text, font, scale)
-    local onScreen <const>, x <const>, y <const> = World3dToScreen2d(x, y, z)
+    local onScreen <const>, x <const>, y <const> = GetScreenCoordFromWorldCoord(x, y, z)
     if not (scale) then
         scale = 1
     end
     if not (font) then
         font = 4
     end
-    if (onScreen) then
-        SetTextScale(0.0 * scale, 0.55 * scale)
-        SetTextFont(font)
-        SetTextProportional(1)
-        SetTextColour(255, 255, 255, 215)
-        SetTextEntry("STRING")
-        SetTextCentre(true)
-        AddTextComponentString(text)
-        DrawText(x, y)
+    if (not onScreen) then
+        return
     end
+
+    SetTextScale(0.0 * scale, 0.55 * scale)
+    SetTextFont(font)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(true)
+    AddTextComponentString(text)
+
+    BeginTextCommandDisplayText("STRING")
+    AddTextComponentString(text)
+    EndTextCommandDisplayText(x, y)
 end
