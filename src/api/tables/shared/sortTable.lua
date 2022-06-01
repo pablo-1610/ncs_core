965d7097f2e5bb95613eecbee4c598b445303561
dedoxtv <unmecpasfort@gmail.com>
2022-06-01 20:07:20 +0200

    ✨ Add some table function

---
+++ src/api/tables/shared/sortTable.lua
@@ -0,0 +1,8 @@
+-- @param tableT table
+-- @param sortFunc function
+-- @return table
+-- @public
+function API_Tables:sortTable(tableT, sortFunc)
+    table.sort(tableT, sortFunc)
+    return tableT
+end
