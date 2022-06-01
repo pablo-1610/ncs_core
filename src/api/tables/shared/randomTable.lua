6b9d769033b1ee6cb7aac24a29e3cfb6f47c431b
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-06-01 11:07:02 +0200

    :art: Code conventions

--- src/api/tables/shared/randomTable.lua
+++ src/api/tables/shared/randomTable.lua
@@ -2,11 +2,13 @@
 ---@param tableR table
 ---@return value
 ---@public
-
 function API_Tables:randomTable(tableR)
-    if not tableR[1] then return end
-    local randM = math.random(0, #tableR)
-    if not tableR[randM] then return end
-  
-    return tableR[randM]
+    if (not (tableR[1])) then
+        return
+    end
+    local randM <const> = math.random(0, #tableR)
+    if not (tableR[randM]) then
+        return
+    end
+    return (tableR[randM])
 end
