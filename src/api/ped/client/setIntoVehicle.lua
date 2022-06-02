9d488bf5e562561c496fc1c26e2286e451275ad0
Dezel <106397570+dezel-2109@users.noreply.github.com>
2022-06-02 18:25:59 +0200

    Update src/api/ped/client/setIntoVehicle.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/api/ped/client/setIntoVehicle.lua
+++ src/api/ped/client/setIntoVehicle.lua
@@ -1,6 +1,7 @@
 ---setIntoVehicle
 ---@param pedId number
 ---@param vehicle number
+---@return void
 ---@public
 function API_Ped:setIntoVehicle(pedId, vehicleId, seat)
     if (not (DoesEntityExist(pedId))) then
