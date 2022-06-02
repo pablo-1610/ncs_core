b1e07245adde02e19ad557b94bd5889a569adf48
ZAKIO <66121290+zakio-dev@users.noreply.github.com>
2022-06-02 20:20:41 +0200

    :hammer: Update from CsokiHUN suggestion
    Co-authored-by: Csoki <36936216+CsokiHUN@users.noreply.github.com>

--- src/core/shared.lua
+++ src/core/shared.lua
@@ -12,23 +12,24 @@ end
 ---@return void
 ---@public
 function _NCS:checkIsUpdate()
-    if GetConvarInt("ncs_allow_version_check", 1) > 0 then
-        PerformHttpRequest("https://raw.githubusercontent.com/NextCitizens/ncs_core/main/fxmanifest.lua", function (_, resultData, _)
-            local currentVersion <const> = _NCS:getVersion()
-            local lines = {}
-
-            for s in resultData:gmatch("[^\r\n]+") do
-                lines[#lines + 1] = s
-            end
-
-            local ver <const> = API_Strings:split(lines[6], "'")[2]
-            if (not (currentVersion == ver)) then
-                _NCS:coreTrace("NCS Core has not up to date ^3please update -> https://github.com/NextCitizens/ncs_core ^7!", _NCSEnum.LogType.ERROR)
-            else
-                _NCS:coreTrace(("NCS Core is up to date ^7! (%s)"):format(currentVersion), _NCSEnum.LogType.INFO)
-            end
-        end)
+    if GetConvarInt("ncs_allow_version_check", 1) <= 0 then
+        return
     end
+    PerformHttpRequest("https://raw.githubusercontent.com/NextCitizens/ncs_core/main/fxmanifest.lua", function (_, resultData, _)
+        local currentVersion <const> = _NCS:getVersion()
+        local lines = {}
+
+        for s in resultData:gmatch("[^\r\n]+") do
+            lines[#lines + 1] = s
+        end
+
+        local ver <const> = API_Strings:split(lines[6], "'")[2]
+        if (currentVersion ~= ver) then
+            _NCS:coreTrace("NCS Core has not up to date ^3please update -> https://github.com/NextCitizens/ncs_core ^7!", _NCSEnum.LogType.ERROR)
+        else
+            _NCS:coreTrace(("NCS Core is up to date ^7! (%s)"):format(currentVersion), _NCSEnum.LogType.INFO)
+        end
+    end)
 end
 
 ---trace
