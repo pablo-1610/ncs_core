0ad4dda7ddf83cbf41cd634f3efb4a6104413ce0
CsokiHUN <feketetibi71@gmail.com>
2022-06-02 09:32:41 +0200

    :adhesive_bandage: add entity validation checking

--- src/api/vehicles/client/cleanVehicle.lua
+++ src/api/vehicles/client/cleanVehicle.lua
@@ -2,6 +2,10 @@
 ---@param vehicleEntity number
 ---@public
 function API_Vehicles:clean(vehicleEntity)
+    if (not (DoesEntityExist(vehicleEntity))) then
+        return _NCS:die("vehicleEntity does not exists")
+    end
+
     SetVehicleDirtLevel(vehicleEntity, 0.0)
     WashDecalsFromVehicle(vehicleEntity, 1.0)
 end
