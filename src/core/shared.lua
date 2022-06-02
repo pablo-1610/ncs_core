37fda1adb928b856dbc97b3aad79b231f27315e1
Ekali <pcnlmmm@gmail.com>
2022-06-02 17:27:04 +0200

    create a func for check update.

--- src/core/shared.lua
+++ src/core/shared.lua
@@ -8,6 +8,26 @@ function _NCS:getVersion()
     return (GetResourceMetadata(GetCurrentResourceName(), "version"))
 end
 
+---checkIsUpdate
+---@return void
+---@public
+function _NCS:checkIsUpdate()
+    PerformHttpRequest("https://raw.githubusercontent.com/NextCitizens/ncs_core/main/fxmanifest.lua", function (_, resultData, _)
+        local currentVersion <const> = _NCS:getVersion()
+        local lines = {}
+
+        for s in resultData:gmatch("[^\r\n]+") do
+            table.insert(lines, s)
+        end
+
+        local ver <const> = API_Strings:split(lines[6], "'")[2]
+
+        if (not (currentVersion == ver)) then
+            _NCS:coreTrace("NCS Core has not up to date ^3please update -> https://github.com/NextCitizens/ncs_core ^7!", _NCSEnum.LogType.ERROR)
+        end
+    end)
+end
+
 ---trace
 ---@param message string
 ---@param logLevelIndex any
