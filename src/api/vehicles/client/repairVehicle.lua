c3c0816f455ea47f35a7e02d3d216ffbe0dbc1bd
CsokiHUN <feketetibi71@gmail.com>
2022-06-02 09:34:05 +0200

    :adhesive_bandage: Add entity validation checking

--- src/api/vehicles/client/repairVehicle.lua
+++ src/api/vehicles/client/repairVehicle.lua
@@ -2,6 +2,10 @@
 ---@param vehicleEntity number
 ---@public
 function API_Vehicles:repair(vehicleEntity)
+    if (not (DoesEntityExist(vehicleEntity))) then
+        return _NCS:die("vehicleEntity does not exists")
+    end
+
     SetVehicleFixed(vehicleEntity)
     SetVehicleDirtLevel(vehicleEntity, 0.0)
     SetVehicleDeformationFixed(vehicleEntity)
