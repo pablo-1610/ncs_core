7da0880d082efa934ddaa43aed9a0f8ffd4f66ed
Tayz <73202029+TayzDev@users.noreply.github.com>
2022-06-02 17:43:18 +0200

    🦹Uptade neverWanted

--- src/api/player/client/neverWanted.lua
+++ src/api/player/client/neverWanted.lua
@@ -1,9 +1,3 @@
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        if GetPlayerWantedLevel(PlayerId()) ~= 0 then -- if player is wanted then
-            SetPlayerWantedLevel(PlayerId(), 0, false) -- set wanted level to 0
-            SetPlayerWantedLevelNow(PlayerId(), false) -- set wanted level to 0 now
-        end
-    end
+SetMaxWantedLevel(0)
 end)
