076bb8c986bf3845d662b2765209832533889296
Dezel <106397570+dezel-2109@users.noreply.github.com>
2022-06-02 18:20:57 +0200

    Update src/api/vehicles/client/openDoor.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/api/vehicles/client/openDoor.lua
+++ src/api/vehicles/client/openDoor.lua
@@ -6,7 +6,7 @@
 ---@public
 function API_Vehicles:openDoor(vehicleId, doorId, canBeClosed, instantly)
     if (vehicleId) and (DoesEntityExist(vehicleId)) then
-    return SetVehicleDoorOpen(vehicleId, doorId, canBeClosed or false, instantly or false)
+        return SetVehicleDoorOpen(vehicleId, doorId, canBeClosed or false, instantly or false)
     end
     _NCS:die("vehicleEntity does not exists")
 end
