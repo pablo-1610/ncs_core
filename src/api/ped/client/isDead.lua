ad40fcaf644eedeec97aecb0762ef17b45b7ec3d
Dezel <106397570+dezel-2109@users.noreply.github.com>
2022-06-02 18:20:07 +0200

    Update src/api/ped/client/isDead.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/api/ped/client/isDead.lua
+++ src/api/ped/client/isDead.lua
@@ -7,9 +7,5 @@ function API_Ped:isDead(pedId)
         return _NCS:die("Target ped does not exists")
     end
 
-    if (API_Ped:getHealth(pedId) <= 0) then
-        return (true)
-    end
-
-    return (false)
+    return (API_Ped:getHealth(pedId) <= 0)
 end
