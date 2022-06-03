8fb8c10b09c41ada6b3ec7476ed5cb6c99eb77df
dezel-2109 <devluafivem@gmail.com>
2022-06-03 21:07:57 +0200

    ✏️ Fix typo & 🔨 Add error

--- src/api/ped/server/deletePed.lua
+++ src/api/ped/server/deletePed.lua
@@ -4,9 +4,11 @@
 ---@public
 function API_Ped:deletePed(pedId)
     if (not (DoesEntityExist(pedId))) then
-        return NCS:die("Target ped does not exists")
+        return NCS:die("Target ped doesn't exist")
     end
     if (IsEntityAPed(pedId)) then
         DeleteEntity(pedId)
+    else
+        return NCS:die("Target ped doesn't exist")
     end
 end
