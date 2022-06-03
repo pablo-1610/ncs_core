8dc9440b66c134fdb177b157b35747880ef901dc
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-06-03 02:27:44 +0200

    :hammer: Add _Interal.OneSync and _Internal.DefaultRoleIdentifier

--- src/modules/config/shared/main.lua
+++ src/modules/config/shared/main.lua
@@ -1,2 +1,4 @@
 _Internal.LogLevel = GetConvarInt("ncs_log_level", 3) or 3
 _Internal.ServerName = GetConvar("sv_hostname", "N/A") or "N/A"
+_Internal.OneSync = (GetConvar("onesync", "false") == "on") or false
+_Internal.DefaultRoleIdentifier = (GetConvar("ncs_default_server_role", "off") == "on")
