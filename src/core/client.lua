0c2f3f7251885b26dc7763f3c42b6b6e305cc2be
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-06-03 02:23:20 +0200

    :hammer: Add NCS:getPlayerData

--- src/core/client.lua
+++ src/core/client.lua
@@ -6,6 +6,13 @@ function _NCS:triggerServerEvent(eventName, ...)
     TriggerServerEvent(self:formatEvent(eventName), ...)
 end
 
+---getPlayerData
+---@return table
+---@public
+function _NCS:getPlayerData()
+    return (MOD_Cache:get(_NCSEnum.CacheType.PLAYER_DATA))
+end
+
 CreateThread(function()
     _NCS:triggerEvent("ncs_core:loaded")
 end)
