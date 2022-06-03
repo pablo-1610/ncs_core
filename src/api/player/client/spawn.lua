f604c31762f72fce6256284f9e433918d3935411
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-06-03 02:20:21 +0200

    :wrench: Add API_Player:spawn

---
+++ src/api/player/client/spawn.lua
@@ -0,0 +1,51 @@
+local defaultModel <const> = "mp_m_freemode_01"
+
+---spawn
+---@param coords table
+---@param callback function
+---@public
+function API_Player:spawn(coords, callback)
+    local position <const>, heading <const> = coords.position, coords.heading
+    CreateThread(function()
+        DoScreenFadeOut(500)
+
+        while (IsScreenFadingOut()) do
+            Wait(0)
+        end
+
+        self:freeze(PlayerId(), true)
+
+        API_Streaming:requestModel(defaultModel)
+        self:setModel(defaultModel)
+
+        SetPedDefaultComponentVariation(PlayerPedId())
+
+        RequestCollisionAtCoord(position.x, position.y, position.z)
+
+        local ped <const> = PlayerPedId()
+
+        -- V requires setting coords as well
+        SetEntityCoordsNoOffset(ped, position.x, position.y, position.z, false, false, false, true)
+        SetEntityHeading(ped, heading)
+
+        NetworkResurrectLocalPlayer(position.x, position.y, position.z, heading, true, true, false)
+
+        ClearPedTasksImmediately(ped)
+        RemoveAllPedWeapons(ped)
+        ClearPlayerWantedLevel(PlayerId())
+
+        ShutdownLoadingScreen()
+
+        DoScreenFadeIn(500)
+
+        while (IsScreenFadingIn()) do
+            Wait(0)
+        end
+
+        self:freeze(PlayerId(), false)
+
+        if (callback) then
+            callback()
+        end
+    end)
+end
