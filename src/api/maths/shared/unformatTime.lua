412284acec55370698dea697e2cfa8c5a8c20b68
Ekali <pcnlmmm@gmail.com>
2022-06-03 20:25:44 +0200

    Update src/api/maths/shared/unformatTime.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/api/maths/shared/unformatTime.lua
+++ src/api/maths/shared/unformatTime.lua
@@ -3,16 +3,11 @@
 ---@return number
 ---@public
 function API_Maths:unformatTime(tblTime)
-    if (not tblTime) then
-        return 0
-    end
-
     local years <const> = tblTime.years or 0
     local months <const> = tblTime.months or 0
     local days <const> = tblTime.days or 0
-    local hrs <const> = tblTime.hours or 0
-    local min <const> = tblTime.minutes or 0
-    local sec <const> = tblTime.seconds or 0
-
-    return ((years * 31536000) + (months * 2592000) + (days * 86400) + (hrs * 3600) + (min * 60) + (sec))
+    local hours <const> = tblTime.hours or 0
+    local minutes <const> = tblTime.minutes or 0
+    local seconds <const> = tblTime.seconds or 0
+    return (years * 31536000 + months * 2592000 + days * 86400 + hours * 3600 + minutes * 60 + seconds)
 end
