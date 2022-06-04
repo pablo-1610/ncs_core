066a28c709a8c40b8de11d2b5b6a921840683853
Ekali <pcnlmmm@gmail.com>
2022-06-04 09:25:20 +0200

    Update src/api/strings/shared/random.lua
    Co-authored-by: Csoki <36936216+CsokiHUN@users.noreply.github.com>

--- src/api/strings/shared/random.lua
+++ src/api/strings/shared/random.lua
@@ -4,6 +4,7 @@
 ---@public
 function API_Strings:randomString(length)
     local charset <const> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    local randomString = ""
     for i = 1, (length or 10) do
         local randomNumber <const> = math.random(1, #charset)
         randomString = randomString .. charset:sub(randomNumber, randomNumber)
