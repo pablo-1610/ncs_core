592314d74a09ad2d2f29096a3618ca3e2c7a97f7
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-06-04 16:34:43 +0200

    :hammer: Ready system for client

--- src/core/client.lua
+++ src/core/client.lua
@@ -14,10 +14,22 @@ function NCS:getPlayerData()
     return (playerData)
 end
 
-CreateThread(function()
-    NCS:triggerEvent("client_ready")
-end)
+---onReady
+---@param callback function
+---@public
+function NCS:onReady(callback)
+    if (self.ready) then
+        callback()
+    else
+        self:handleEvent("clientReady", callback)
+    end
+end
 
 NCS:handleEvent("cacheSet", function(cacheType, firstTime)
-    print(cacheType, firstTime)
+    if (not (NCS.ready)) then
+        if (cacheType == NCSEnum.CacheType.PLAYER_DATA and firstTime) then
+            NCS.ready = true
+            NCS:triggerEvent("clientReady")
+        end
+    end
 end)
