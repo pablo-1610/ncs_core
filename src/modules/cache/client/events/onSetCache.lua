8a937b3feca9feaf071cf349acbb1407267af1ad
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-06-04 16:34:08 +0200

    :coffin: Remove test

--- src/modules/cache/client/events/onSetCache.lua
+++ src/modules/cache/client/events/onSetCache.lua
@@ -1,10 +1,3 @@
 NCS:registerNetEvent("setCache", function(cacheType, data)
     MOD_Cache:set(cacheType, data)
 end)
-
-CreateThread(function()
-    while (true) do
-        Wait(1200)
-        print(MOD_Cache:exists(NCSEnum.CacheType.PLAYER_DATA))
-    end
-end)
