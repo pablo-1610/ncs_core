eea5ed481c261645635fa6e8129af74f4e697336
MoskalykA <streamarthur462@gmail.com>
2022-06-05 23:10:25 +0200

    ⚡️ table.insert -> # + 1 = ...

--- src/class/NCSAdaptiveCardBuilder/server/functions/addTextBloc.lua
+++ src/class/NCSAdaptiveCardBuilder/server/functions/addTextBloc.lua
@@ -3,10 +3,10 @@
 ---@return NCSAdaptiveCardBuilder
 ---@public
 function NCSAdaptiveCardBuilder:addTextBloc(text)
-    table.insert(self.payload.body, {
+    self.payload.body[#self.payload.body + 1] = {
         type = "TextBlock",
         text = text,
         wrap = true
-    })
+    }
     return (self)
 end
--- src/class/NCSAdaptiveCardBuilder/server/functions/addTitle.lua
+++ src/class/NCSAdaptiveCardBuilder/server/functions/addTitle.lua
@@ -3,11 +3,11 @@
 ---@return NCSAdaptiveCardBuilder
 ---@public
 function NCSAdaptiveCardBuilder:addTitle(text)
-    table.insert(self.payload.body, {
+    self.payload.body[#self.payload.body + 1] = {
         weight = "Bolder",
         type = "TextBlock",
         text = text,
         size = "Medium"
-    })
+    }
     return (self)
 end
