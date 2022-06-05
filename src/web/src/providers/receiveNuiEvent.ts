cfdeefd57c845215734e077f24388e8d6c00205e
MoskalykA <streamarthur462@gmail.com>
2022-06-05 22:47:07 +0200

    🎨 Passes from ' to "

--- src/web/src/providers/receiveNuiEvent.ts
+++ src/web/src/providers/receiveNuiEvent.ts
@@ -1,16 +1,16 @@
-import {useEffect} from 'react'
+import {useEffect} from "react"
 
-const receiveNuiEvent = (moduleName: String, functionName: String, moduleCallback: Function) => {
+const receiveNuiEvent = (moduleName: string, functionName: string, moduleCallback: Function) => {
    useEffect(() => {
       const eventHandler = (event: MessageEvent) => {
-         const data: { moduleName: String, functionName: String, argsList: Object } = event.data
+         const data: { moduleName: string, functionName: string, argsList: Object } = event.data
          if (data.moduleName === moduleName && data.functionName === functionName) {
             moduleCallback(data.argsList)
          }
       }
 
-      window.addEventListener('message', eventHandler)
-      return () => window.removeEventListener('message', eventHandler)
+      window.addEventListener("message", eventHandler)
+      return () => window.removeEventListener("message", eventHandler)
    }, [])
 }
 
