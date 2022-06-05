94f913ed089b951111a38ac9349e0cc3e81df369
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-06-05 14:57:16 +0200

    :hammer: Add divider task for zones module

---
+++ src/modules/zones/client/tasks/divider.lua
@@ -0,0 +1,33 @@
+local splitDistance <const> = GetConvarInt("ncs_zones_split_distance", 100)
+
+function MOD_Zones:startDivider()
+    CreateThread(function()
+        while (true) do
+            local coords <const> = GetEntityCoords(PlayerPedId())
+
+            -- Check if the zone is too far from the player
+            ---@param zone NCSZone
+            for id, zone in pairs(self.drawing) do
+                if (#(coords - zone.position) > splitDistance) then
+                    print(("^1Unsubscribing ^7from zone : %s"):format(zone.id))
+                    self.drawing[id] = nil
+                end
+            end
+
+            -- Check if the zone is in the player's range
+            ---@param zone NCSZone
+            for id, zone in pairs(self.list) do
+                if (self.drawing[id]) then
+                    goto continue
+                end
+                if (#(coords - zone.position) <= splitDistance) then
+                    print(("^2Subscribing ^7to zone : %s"):format(zone.id))
+                    self.drawing[id] = zone
+                end
+                :: continue ::
+            end
+
+            Wait(2000)
+        end
+    end)
+end
