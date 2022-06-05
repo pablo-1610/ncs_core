8cda0ffff9908104f49e95b3ba3d385549432c86
MoskalykA <streamarthur462@gmail.com>
2022-06-05 22:48:44 +0200

    🎨 Passes from ' to "

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './assets/index.css'
-import Visibility from './providers/visibility'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Visibility from "./providers/visibility"
+import HUD from "./components/HUD"
+import "./assets/index.css"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <>
-      <Visibility moduleName="core" visibleComponent={<App />} />
+      <Visibility moduleName="hud" visibleComponent={<HUD />} alwaysVisible={true} />
     </>
   </React.StrictMode>
 )
