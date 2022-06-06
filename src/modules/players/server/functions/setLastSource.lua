367b522225efca5fe5d27feaad0194917f137f13
CsokiHUN <feketetibi71@gmail.com>
2022-06-06 21:57:52 +0200

    :recycle: replace query with update function

--- src/modules/players/server/functions/setLastSource.lua
+++ src/modules/players/server/functions/setLastSource.lua
@@ -3,7 +3,7 @@
 ---@param source number
 ---@public
 function MOD_Players:setLastSource(identifier, source)
-    API_Database:query("UPDATE ncs_players SET last_source = @last_source WHERE player_identifier = @player_identifier", {
+    API_Database:update("UPDATE ncs_players SET last_source = @last_source WHERE player_identifier = @player_identifier", {
         ["@last_source"] = source,
         ["@player_identifier"] = identifier
     })
