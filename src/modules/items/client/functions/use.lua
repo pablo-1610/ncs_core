102e33f950ea7d9999e8a11471512854aa4b34b6
Wz <68116801+Wz-FiveM@users.noreply.github.com>
2022-06-06 12:18:20 +0200

    :pencil2: Update src/modules/items/client/functions/use.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/modules/items/client/functions/use.lua
+++ src/modules/items/client/functions/use.lua
@@ -7,6 +7,6 @@ function MOD_Items:use(name)
     if MOD_Items:getCanUse(name) then
         return self.usable[name].callback()
     else
-        NCS:die('Item <' .. name .. '> can\'t be used.')
+        NCS:die(("Item <%s> can't be used."):format(name))
     end
 end
