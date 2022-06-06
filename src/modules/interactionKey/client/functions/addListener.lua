f4cb107b18c93cbb8e86e859087d1ca8bfea5590
MoskalykA <streamarthur462@gmail.com>
2022-06-06 16:29:14 +0200

    ⚡️ table.insert -> # + 1 = ...

--- src/modules/interactionKey/client/functions/addListener.lua
+++ src/modules/interactionKey/client/functions/addListener.lua
@@ -3,5 +3,5 @@
 ---@return number
 ---@public
 function MOD_InteractionKey:addListener(handler)
-    return (table.insert(MOD_InteractionKey.listeners, handler))
+    MOD_InteractionKey.listeners[#MOD_InteractionKey.listeners + 1] = handler
 end
