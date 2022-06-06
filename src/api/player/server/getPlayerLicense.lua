5af0605fdc9ee5a497f5ef8c91ea6f94710543ed
Dezel <106397570+dezel-2109@users.noreply.github.com>
2022-06-06 13:08:19 +0200

    Update src/api/player/server/getPlayerLicense.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/api/player/server/getPlayerLicense.lua
+++ src/api/player/server/getPlayerLicense.lua
@@ -4,7 +4,7 @@
 ---@public
 function API_Player:getLicense(playerSrc)
     if (not playerSrc or playerSrc <= 0) then
-        return NCS:die('Unable to find license : source is invalid')
+        return NCS:die("Unable to find license : source is invalid")
     end
     for _, v in pairs(GetPlayerIdentifiers(playerSrc)) do
         if (string.sub(v, 1, string.len("license:")) == "license:") then
