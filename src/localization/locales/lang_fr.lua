94afafcc9ba2fb8c6c194e8f78dd3614cea2fa38
L4 <83190245+LQuatre@users.noreply.github.com>
2022-06-06 22:41:25 +0200

    Update lang_fr.lua

--- src/localization/locales/lang_fr.lua
+++ src/localization/locales/lang_fr.lua
@@ -41,7 +41,6 @@ _Literals = {
 
     BIND_INTERACTION_KEY = "Intéragir",
     
-    
     ERROR_PLAYER_NOT_DEAD = "Le joueur n'est pas mort.",
     ERROR_PLAYER_ALREADY_DEAD = "Le joueur est déjà mort.",
 }
