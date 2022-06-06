2f5a331585a88fec789aded65e9ad0aec5d648e6
Dezel <106397570+dezel-2109@users.noreply.github.com>
2022-06-06 13:08:02 +0200

    Update src/api/player/server/getPlayerIdentifier.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/api/player/server/getPlayerIdentifier.lua
+++ src/api/player/server/getPlayerIdentifier.lua
@@ -4,7 +4,7 @@
 ---@public
 function API_Player:getIdentifier(playerSrc)
     if (not playerSrc or playerSrc <= 0) then
-        return NCS:die('Unable to find identifier : source is invalid')
+        return NCS:die("Unable to find identifier : source is invalid")
     end
     return (self:getIdentifiers(playerSrc)[MOD_Config:getIdentificationType()])
 end
