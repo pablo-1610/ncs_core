fa9d2e244e54c345bb0c0d5da446dd43a1c88965
Pablo Zapata <pablo.zapata.dev@gmail.com>
2022-06-06 18:47:18 +0200

    :hammer: Update MOD_Time:startClock

--- src/modules/time/server/tasks/clock.lua
+++ src/modules/time/server/tasks/clock.lua
@@ -4,15 +4,15 @@ function MOD_Time:startClock()
     if (self.running) then
         return
     end
-    self.running = true
+    MOD_Time.running = true
     CreateThread(function()
         while (self.running) do
-            self.currentTime[2] = self.currentTime[2] + 1
-            if (self.currentTime[2] >= 60) then
-                self.currentTime[2] = 0
-                self.currentTime[1] = self.currentTime[1] + 1
-                if (self.currentTime[1] >= 24) then
-                    self.currentTime[1] = 0
+            MOD_Time.currentTime[2] = MOD_Time.currentTime[2] + 1
+            if (MOD_Time.currentTime[2] >= 60) then
+                MOD_Time.currentTime[2] = 0
+                MOD_Time.currentTime[1] = MOD_Time.currentTime[1] + 1
+                if (MOD_Time.currentTime[1] >= 24) then
+                    MOD_Time.currentTime[1] = 0
                 end
             end
             NCS:triggerClientEvent("updateTime", -1, self.currentTime)
