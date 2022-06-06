a66013e10d19c7a1b2ab264763de0b17b75e16ba
Dezel <106397570+dezel-2109@users.noreply.github.com>
2022-06-06 13:08:12 +0200

    Update src/api/player/server/getPlayerIdentifiers.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/api/player/server/getPlayerIdentifiers.lua
+++ src/api/player/server/getPlayerIdentifiers.lua
@@ -4,7 +4,7 @@
 ---@public
 function API_Player:getIdentifiers(playerSrc)
 	if (not playerSrc or playerSrc <= 0) then
-		return NCS:die('Unable to find identifiers : source is invalid')
+		return NCS:die("Unable to find identifiers : source is invalid")
 	end
 	local identifiers = {}
 	for _, v in pairs(GetPlayerIdentifiers(playerSrc)) do
