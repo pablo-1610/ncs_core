1fef0f1b32bd00ecb4550ae3584d13240ceea554
MoskalykA <streamarthur462@gmail.com>
2022-06-06 13:28:27 +0200

    🐛 Correction of id

--- src/modules/sanctions/server/functions/warnPlayer.lua
+++ src/modules/sanctions/server/functions/warnPlayer.lua
@@ -3,16 +3,14 @@
 ---@param reason string
 ---@return void
 function MOD_Sanctions:warnPlayer(license, reason)
-    local warn_id <const> = ("WARN-%s-%s"):format(API_Strings:randomString(3), API_Strings:randomString(5))
-
-    local query <const> = ("INSERT INTO ncs_warns (warn_id, license, reason, date) VALUES ('%s', '%s', '%s', '%i')"):format(warn_id, license, reason, os.time())
+    local query <const> = ("INSERT INTO ncs_warns (license, reason, date) VALUES ('%s', '%s', '%i')"):format(license, reason, os.time())
     API_Database:query(query, {}, function(result)
         if (not result) then
             NCS:trace(("Failed to insert warn for player %s."):format(license))
             return
         end
-        MOD_Sanctions.List.Warns[warn_id] = {
-            ["warn_id"] = warn_id,
+        MOD_Sanctions.List.Warns[result.insertId] = {
+            ["id"] = result.insertId,
             ["license"] = license,
             ["reason"] = reason,
             ["date"] = date
