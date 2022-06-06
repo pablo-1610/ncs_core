bcf3fe37aedbf561195f0662ce96ec3b8da07bad
Dezel <106397570+dezel-2109@users.noreply.github.com>
2022-06-06 13:07:54 +0200

    Update src/api/player/server/getPlayerName.lua
    Co-authored-by: MoskalykA <100430077+MoskalykA@users.noreply.github.com>

--- src/api/player/server/getPlayerName.lua
+++ src/api/player/server/getPlayerName.lua
@@ -6,9 +6,5 @@ function API_Player:getName(playerSrc)
     if (not playerSrc or playerSrc <= 0) then
         return NCS:die('Unable to find name : source is invalid')
     end
-    ---@type string
-    local name <const> = GetPlayerName(playerSrc)
-    if (name) then
-        return (name)
-    end
+    return GetPlayerName(playerSrc)
 end
