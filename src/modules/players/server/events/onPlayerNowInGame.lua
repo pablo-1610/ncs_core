955d800d2e56495089084f50cc327c20f07d1de1
dedoxtv <unmecpasfort@gmail.com>
2022-06-07 23:36:33 +0200

    fix triggerClientEvent / delete onPlayerDeath move on onPlayerNowInGame

--- src/modules/players/server/events/onPlayerNowInGame.lua
+++ src/modules/players/server/events/onPlayerNowInGame.lua
@@ -69,4 +69,53 @@ NCS:registerNetEvent("nowInGame", function()
     end
 
     check()
+
+    local isDead = 0
+    local waitTime = 1000
+
+    Wait(waitTime*5)
+    if (MOD_Players:exists(_src)) then
+        local player = MOD_Players:get(_src)
+        if (player.character.isDead ~= 0) then
+            NCS:triggerClientEvent("playerIsNowDeath", _src)
+        end
+    end
+
+    while true do
+        if (MOD_Players:exists(_src)) then
+            local playerPed <const> = GetPlayerPed(_src)
+            if ((GetEntityHealth(playerPed) <= 0) and (isDead == 0)) then
+                waitTime = 100
+                isDead = 1
+                
+                local sourceKillerEntity <const>, sourceDeathCause <const> = GetPedSourceOfDeath(playerPed), GetPedCauseOfDeath(playerPed)
+                local playerDeathCoords <const> = GetEntityCoords(playerPed)
+                if sourceKillerEntity ~= _src and sourceKillerEntity  ~= 0 then
+                    local playerDeathData = {
+                        ["playerDeathCoords"] = playerDeathCoords,
+                        ["playerDeathCause"] = sourceDeathCause,
+                        ["playerDeathTime"] = GetGameTimer(),
+                        ["killerEntity"] = sourceKillerEntity
+                    }
+                    NCS:trace(("Player id ^2%s ^7(^2%s^7) as been killed by ^2%s ^7(^2%s^7)"):format(_src, MOD_Players:get(_src).name, sourceKillerEntity, MOD_Players:get(sourceKillerEntity).name), 3)
+                    NCSCharacter:setDeathStatus(_src, isDead, playerDeathData)
+                    TriggerEvent("playerIsNowDeath", _src, playerDeathData)
+                else
+                    local playerDeathData = {
+                        ["playerDeathCoords"] = playerDeathCoords,
+                        ["playerDeathCause"] = sourceDeathCause,
+                        ["playerDeathTime"] = GetGameTimer(),
+                        ["killerEntity"] = false
+                    }
+                    NCS:trace(("Player id ^2%s ^7(^2%s^7) died without killer"):format(_src, MOD_Players:get(_src).name), 3)
+                    NCSCharacter:setDeathStatus(_src, isDead, playerDeathData)
+                    NCS:triggerEvent("playerIsNowDeath", _src, playerDeathData)
+                end
+            elseif (GetEntityHealth(playerPed) >= 1) and (isDead == 1) then
+                waitTime = 1000
+                isDead = 0
+            end
+        end
+        Wait(waitTime)
+    end
 end)
