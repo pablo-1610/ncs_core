b63d24f42ce8e7dc7b7e49883529b64fd18681ce
dedoxtv <unmecpasfort@gmail.com>
2022-06-07 22:56:08 +0200

    Update save.lua

--- src/class/NCSCharacter/server/functions/save.lua
+++ src/class/NCSCharacter/server/functions/save.lua
@@ -2,13 +2,13 @@
 ---@public
 ---@param self NCSCharacter
 function NCSCharacter.save(self)
-    API_Database:update("UPDATE ncs_players_characters SET skin = @skin WHERE character_id = @character_id", {
+    API_Database:update("UPDATE ncs_players_characters SET skin = @skin, is_dead = @is_dead WHERE character_id = @character_id", {
         ["@character_id"] = self.id,
         ["@skin"] = json.encode(self.skin),
         ["@accounts"] = json.encode(self.accounts),
         ["@lastPosition"] = json.encode(self.lastPosition),
         ["@metadata"] = json.encode(self.metadata),
         ["@is_dead"] = self.isDead,
-        ["@death_data"] = json.encode(self.deathData)
+        ["@death_cause"] = json.encode(self.deathData)
     })
 end 
