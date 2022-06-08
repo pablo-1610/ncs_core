83a90cf628cc4bf5723811d11366f2474349fd2c
Barwoz.Dev <85193395+Barwoz@users.noreply.github.com>
2022-06-08 17:28:30 +0200

    Command SpawnCar

--- src/modules/commands/server/car.lua
+++ src/modules/commands/server/car.lua
@@ -5,18 +5,18 @@ local colorCar = {
     white = {255, 255, 255},
     black = {0, 0, 0},
     yellow = {255, 255, 0},
-    orange = {255, 128, 0},
+    orange = {255, 123, 0},
     pink = {255, 0, 255},
-    purple = {128, 0, 255},
-    brown = {128, 64, 0},
+    purple = {60, 0, 255},
+    brown = {153, 102, 51},
     grey = {128, 128, 128},
 }
 
 API_Commands:registerPermissionCommand('car', 'canCar', function(player, args)
-    if #args >= 3 then 
+    if (#args >= 3) then 
         return (player:showSystemNotification(_Literals.ERROR_TOO_MANY_ARGS, NCSEnum.LogType.ERROR))
     end 
-    if args[1] == nil then 
+    if (args[1] == nil) then 
         return (player:showSystemNotification(_Literals.ERROR_CAR_NO_MODEL, NCSEnum.LogType.ERROR))
     end
     local function spawnVehicle(colorVeh)
@@ -30,7 +30,7 @@ API_Commands:registerPermissionCommand('car', 'canCar', function(player, args)
         NCS:triggerClientEvent("ncs_core:setVehicleProperties", player.serverId, args[1], playerCoords, playerHeading, props)
         return (player:showSystemNotification(_Literals.SUCCESS_CAR_SPAWNED, NCSEnum.LogType.SUCCESS))
     end
-    if args[2] == nil then 
+    if (args[2] == nil) then 
         local r, g, b = math.random(255), math.random(255), math.random(255)
         spawnVehicle({r, g, b})
     else 
