4f4d4e62dd1b266b2ef111b759cfaeb33938116f
CsokiHUN <feketetibi71@gmail.com>
2022-06-11 21:46:04 +0200

    :recycle: update setPosition param

--- src/api/camera/client/setPosition.lua
+++ src/api/camera/client/setPosition.lua
@@ -1,10 +1,10 @@
 ---setPosition
----@param coords table<number, number, number>
+---@param position table<number, number, number>
 ---@public
-function NCSCamera:setPosition(coords)
+function NCSCamera:setPosition(position)
     if (not self.exist) then
         return
     end
 
-    SetCamCoord(self.id, coords)
+    SetCamCoord(self.id, position)
 end
