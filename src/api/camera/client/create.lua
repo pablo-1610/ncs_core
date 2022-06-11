0f6191db85d7db972b4d051a2cbab0330724940d
CsokiHUN <feketetibi71@gmail.com>
2022-06-11 21:34:57 +0200

    :truck: add camera API - create.lua

---
+++ src/api/camera/client/create.lua
@@ -0,0 +1,22 @@
+---@class NCSCamera
+---@field public id string
+NCSCamera = {}
+NCSCamera.__index = NCSCamera
+
+---create
+---@param coords table<number, number, number>
+---@param heading number
+---@return table
+---@public
+function API_Camera:create(coords, heading)
+    local self = {}
+    self.id = CreateCam("DEFAULT_SCRIPTED_CAMERA", 0)
+    self.exist = DoesCamExist(self.id)
+
+    SetCamCoord(self.id, coords)
+    if cameraHeading then
+        PointCamAtCoord(self.id, heading)
+    end
+
+    return setmetatable(self, NCSCamera)
+end
