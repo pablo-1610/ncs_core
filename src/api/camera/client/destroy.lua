f564da65128bd419db472915b188e648a2d29a4d
CsokiHUN <feketetibi71@gmail.com>
2022-06-11 21:37:58 +0200

    :adhesive_bandage: fix

--- src/api/camera/client/destroy.lua
+++ src/api/camera/client/destroy.lua
@@ -1,10 +1,10 @@
 ---destroy
 ---@public
 function NCSCamera:destroy()
-    if not self.exist then
+    if (not self.exist) then
         return
     end
-    
+
     SetCamActive(self.id, false)
     RenderScriptCams(false, true, 3000, true, true)
     DestroyCam(self.id, false)
