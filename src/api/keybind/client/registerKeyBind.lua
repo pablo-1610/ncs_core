2b60c99c518a2db4469429bf18887ec1d476b9e1
ach <82474261+ach-git@users.noreply.github.com>
2022-06-12 21:33:07 +0200

    Fix last PR

--- src/api/keybind/client/registerKeyBind.lua
+++ src/api/keybind/client/registerKeyBind.lua
@@ -7,9 +7,6 @@
 function API_KeyBind:bind(name, defaultKey, desc, callback)
     local cmd = ("ncs_%s"):format(name)
     RegisterCommand(cmd, function()
-        if (not (self.enabled)) then
-            return
-        end
         callback()
     end)
     RegisterKeyMapping(cmd, desc, "keyboard", defaultKey)
