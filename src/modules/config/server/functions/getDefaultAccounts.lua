23c036c84c566104bd285481064140bf67cb8ecb
MoskalykA <streamarthur462@gmail.com>
2022-06-12 19:43:57 +0200

    style: better visibility

--- src/modules/config/server/functions/getDefaultAccounts.lua
+++ src/modules/config/server/functions/getDefaultAccounts.lua
@@ -2,5 +2,8 @@
 ---@return table
 ---@public
 function MOD_Config:getDefaultAccounts()
-    return ({ cash = GetConvarInt("ncs_base_cash_money", 0), blackMoney = GetConvarInt("ncs_base_black_money", 0) })
+    return ({ 
+        cash = GetConvarInt("ncs_base_cash_money", 0), 
+        blackMoney = GetConvarInt("ncs_base_black_money", 0) 
+    })
 end
