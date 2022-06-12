2fda00b73e397868798accd2d437388dbf5db912
MoskalykA <streamarthur462@gmail.com>
2022-06-12 19:15:58 +0200

    refactor: more pragmatic variable name

--- src/modules/nosql/server/functions/getIsContentAvailable.lua
+++ src/modules/nosql/server/functions/getIsContentAvailable.lua
@@ -1,8 +1,8 @@
----getIsContentAvailable
+---contentIsAvailable
 ---@param fileName string
 ---@return string
 ---@public
-function MOD_NoSQL:getIsContentAvailable(fileName)
+function MOD_NoSQL:contentIsAvailable(fileName)
     local resPath <const> = GetResourcePath(NCS.resourceName)
     local filePath <const> = ("%s%s.json"):format(("%s/data/"):format(resPath), fileName)
     return (self:fileExists(filePath))
--- src/modules/time/server/events/onReady.lua
+++ src/modules/time/server/events/onReady.lua
@@ -2,7 +2,7 @@ local dataName <const> = "time"
 
 NCS:onReady(function()
     local content <const> = NCS.NoSQL:getContent(dataName)
-    if (not (NCS.NoSQL:getIsContentAvailable(dataName)) or not content) then
+    if (not (NCS.NoSQL:contentIsAvailable(dataName)) or not content) then
         MOD_Time.currentTime = { 12, 00 }
     elseif content then
         MOD_Time.currentTime = content
