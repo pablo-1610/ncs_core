e9e602773ad27430e68a40e9c0ca4d3b0f65a6a5
MoskalykA <streamarthur462@gmail.com>
2022-06-12 19:35:34 +0200

    bug: attempt to call a nil value (method 'setPlayer')

--- src/modules/players/server/events/onPlayerInitialized.lua
+++ src/modules/players/server/events/onPlayerInitialized.lua
@@ -9,8 +9,8 @@ NCS:registerNetEvent("initializedCharacter", function()
             if ((GetEntityHealth(playerPed) <= 0) and (isDead == 0)) then
                 waitTime = 100
                 isDead = 1
-                local player = MOD_Players:getPlayer(_src)
-                
+
+                local player = MOD_Players:get(_src)
                 local sourceKillerEntity <const>, sourceDeathCause <const> = GetPedSourceOfDeath(playerPed), GetPedCauseOfDeath(playerPed)
                 local playerDeathCoords <const> = GetEntityCoords(playerPed)
                 if sourceKillerEntity ~= _src and sourceKillerEntity  ~= 0 then
@@ -23,7 +23,7 @@ NCS:registerNetEvent("initializedCharacter", function()
                     NCS:trace(("Player id ^2%s ^7(^2%s^7) as been killed by ^2%s ^7(^2%s^7)"):format(_src, MOD_Players:get(_src).name, sourceKillerEntity, MOD_Players:get(sourceKillerEntity).name), 3)
                     player.character:setDeathStatus(isDead, playerDeathData)
                     TriggerEvent("playerIsNowDeath", _src, playerDeathData)
-                    MOD_Players:setPlayer(_src, player)
+                    MOD_Players:set(_src, player)
                 else
                     local playerDeathData = {
                         ["playerDeathCoords"] = playerDeathCoords,
@@ -34,7 +34,7 @@ NCS:registerNetEvent("initializedCharacter", function()
                     NCS:trace(("Player id ^2%s ^7(^2%s^7) died without killer"):format(_src, MOD_Players:get(_src).name), 3)
                     player.character:setDeathStatus(isDead, playerDeathData)
                     NCS:triggerEvent("playerIsNowDeath", _src, playerDeathData)
-                    MOD_Players:setPlayer(_src, player)
+                    MOD_Players:set(_src, player)
                 end
             elseif (GetEntityHealth(playerPed) >= 1) and (isDead == 1) then
                 waitTime = 1000
