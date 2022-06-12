c9223b1a5af141c1d5d94ed9b9b8905283e4502b
ach <82474261+ach-git@users.noreply.github.com>
2022-06-12 10:55:44 +0200

    Undo useless things

--- src/api/discordBot/_export.lua
+++
@@ -1,1 +0,0 @@
-_G.NCS.DiscordBot = API_DiscordBot
--- src/api/player/client/showStatsNotification.lua
+++ src/api/player/client/showStatsNotification.lua
@@ -6,7 +6,7 @@
 ---@param newProgress number
 ---@public
 function API_Player:showStatsNotification(icon, title, subTitle, oldProgress, newProgress)
-    local textureDict <const> = API_Streaming:requestStreamedTextureDict(icon)
+    API_Streaming:requestStreamedTextureDict(icon)
     BeginTextCommandThefeedPost("STRING")
     AddTextComponentSubstringPlayerName(subTitle)
     AddTextEntry(title,title)
