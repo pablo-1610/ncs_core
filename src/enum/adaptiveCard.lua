1ff8ba8aebabc058aa009c1805889d3c938b4504
CsokiHUN <feketetibi71@gmail.com>
2022-06-20 12:13:23 +0200

    :fire: remove old enum

--- src/enum/adaptiveCard.lua
+++
@@ -1,4 +0,0 @@
-NCSEnum.AdaptiveCard = {
-    -- TODO : Remove this shit, then use the NCSAdaptiveCardBuilder class
-    CONNECTION_NO_CHARACTERS = [[{"type":"AdaptiveCard","body":[{"type":"TextBlock","size":"Medium","weight":"Bolder","text":"%s"},{"type":"TextBlock","wrap":true,"text":"%s"},{"type":"ActionSet","actions":[{"type":"Action.Submit","id":"create","title":"%s"}],"id":"creation"}],"$schema":"http://adaptivecards.io/schemas/adaptive-card.json","version":"1.5"}]],
-}
