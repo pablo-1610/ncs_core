d54ee3e54e4760debcdd19edda6dd6c765ff7d1c
MoskalykA <streamarthur462@gmail.com>
2022-07-05 22:05:34 +0200

    refactor: use of constants when necessary

--- src/modules/commands/server/revivePlayer.lua
+++ src/modules/commands/server/revivePlayer.lua
@@ -1,14 +1,14 @@
 API_Commands:registerPermissionCommand("revive", "canRevive", function(player, args)
     local firstArgs <const> = args[1]
-    local target = MOD_Players:get(firstArgs == nil and player.serverId or firstArgs)
+    local target <const> = MOD_Players:get(firstArgs == nil and player.serverId or firstArgs)
     if not target then
         player:showSystemNotification(_Literals.TYPE_ERROR, NCSEnum.LogType.ERROR)
         return
     end
 
-    local playerPed = GetPlayerPed(target.serverId)
-    local coords = GetEntityCoords(playerPed)
-	local formattedCoords = {
+    local playerPed <const> = GetPlayerPed(target.serverId)
+    local coords <const> = GetEntityCoords(playerPed)
+	local formattedCoords <const> = {
 		position = vector3(coords.x, coords.y, coords.z),
 		heading = 90.0
 	}
