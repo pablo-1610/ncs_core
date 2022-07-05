283f4f79bb335f226a21fc0ff9dd4409c1c0538d
utopi_160 <93077305+utopi160@users.noreply.github.com>
2022-07-05 08:04:50 +0200

    Update save.lua

--- src/class/NCSCharacter/server/functions/save.lua
+++ src/class/NCSCharacter/server/functions/save.lua
@@ -4,14 +4,16 @@
 ---@param self NCSCharacter
 ---@param callback function
 function NCSCharacter.save(self, callback)
-    API_Database:update("UPDATE ncs_players_characters, ncs_inventories SET ncs_players_characters.skin = @skin, ncs_players_characters.is_dead = @is_dead, ncs_players_characters.death_cause = @death_cause, ncs_inventories.max_weight = @max_weight, ncs_inventories.allowed_content = @allowed_content, ncs_inventories.accounts = @accounts, ncs_inventories.weapons = @weapons, ncs_inventories.items = @items WHERE ncs_players_characters.character_id = @character_id and ncs_inventories.character_id = @character_id",{
+    API_Database:update("UPDATE ncs_players_characters, ncs_inventories SET ncs_players_characters.skin = @skin, ncs_players_characters.is_dead = @is_dead, ncs_players_characters.death_cause = @death_cause, ncs_players_characters.job = @job, ncs_players_characters.job_grade = @job_grade, ncs_inventories.max_weight = @max_weight, ncs_inventories.allowed_content = @allowed_content, ncs_inventories.accounts = @accounts, ncs_inventories.weapons = @weapons, ncs_inventories.items = @items WHERE ncs_players_characters.character_id = @character_id and ncs_inventories.character_id = @character_id",{
         ["@character_id"] = self.id,
         ["@skin"] = json.encode(self.skin),
         ["@lastPosition"] = json.encode(self.lastPosition),
         ["@metadata"] = json.encode(self.metadata),
         ["@is_dead"] = self.isDead,
         ["@death_cause"] = json.encode(self.deathData),
-
+        ["@job"] = self.job,
+        ["@job_grade"] = self.job_grade,
+        
         ["@max_weight"] = tonumber(self.inventory.maxWeight),
         ["@allowed_content"] = json.encode(self.inventory.allowedContent or {}),
         ["@accounts"] = json.encode(self.inventory.accounts),
@@ -23,3 +25,5 @@ function NCSCharacter.save(self, callback)
         end
     end)
 end 
+
+
