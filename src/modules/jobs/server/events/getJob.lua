825e649ee8a886ad26f5c825c6fe3f6bcfa995da
MoskalykA <streamarthur462@gmail.com>
2022-07-05 22:12:22 +0200

    refactor: use of constants when necessary

--- src/modules/jobs/server/events/getJob.lua
+++ src/modules/jobs/server/events/getJob.lua
@@ -1,6 +1,6 @@
 API_Callback:register("getPlayerJob", function(source)
-    local character = MOD_Players:get(source).character
-    local job = character:getJob()
-    local jobGrade = character:getJobGrade()
+    local character <const> = MOD_Players:get(source).character
+    local job <const> = character:getJob()
+    local jobGrade <const> = character:getJobGrade()
     return job, jobGrade
 end)
