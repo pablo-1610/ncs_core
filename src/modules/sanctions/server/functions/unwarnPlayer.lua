d031a1290cddd0b8ef41a6db7252f18a946b9750
MoskalykA <streamarthur462@gmail.com>
2022-07-05 22:20:13 +0200

    refactor: change of variable name

--- src/modules/sanctions/server/functions/unwarnPlayer.lua
+++ src/modules/sanctions/server/functions/unwarnPlayer.lua
@@ -1,20 +1,22 @@
 ---unwarnPlayer
----@param warn_id string
+---@param warnId string
 ---@param license string
 ---@return void
-function MOD_Sanctions:unwarnPlayer(warn_id, license)
-    if (not warn_id or not license) then
-        NCS:trace("You need to specify a warn_id and a license.")
+function MOD_Sanctions:unwarnPlayer(warnId, license)
+    if (not warnId or not license) then
+        NCS:trace("You need to specify a warnId and a license.")
         return
     end
-    local query <const> = ("DELETE FROM ncs_warns WHERE id = '%s' AND license = '%s'"):format(warn_id, license)
+
+    local query <const> = ("DELETE FROM ncs_warns WHERE id = '%s' AND license = '%s'"):format(warnId, license)
     API_Database:query(query, {}, function(result)
         if (not result) then
             NCS:trace(("Failed to delete warn for player %s."):format(license))
             return
         end
+        
         NCS:trace(("Player %s unwarned."):format(license))
 
-        MOD_Sanctions.List.Warns[warn_id] = nil
+        MOD_Sanctions.List.Warns[warnId] = nil
     end)
 end
