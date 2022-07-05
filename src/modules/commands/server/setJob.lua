f513a49ade2f04e9b465b3e2e109bd8ef6ce0f41
MoskalykA <streamarthur462@gmail.com>
2022-07-05 21:59:25 +0200

    refactor: use of variable when necessary

--- src/modules/commands/server/setJob.lua
+++ src/modules/commands/server/setJob.lua
@@ -1,31 +1,34 @@
 API_Commands:registerPermissionCommand("setjob", "canSetJob", function(player, args)
-    if not args[1] then
+    local firstArgs <const> = args[1]
+    local secondArgs <const> = args[2]
+    local thirdArgs <const> = args[3]
+    if not firstArgs then
         player:showSystemNotification(_Literals.ERROR_NO_PLAYER_ID, NCSEnum.LogType.ERROR)
         return
     end 
     
-    local target = MOD_Players:get(args[1])
+    local target = MOD_Players:get(firstArgs)
     if not target then
         player:showSystemNotification(_Literals.TYPE_ERROR, NCSEnum.LogType.ERROR)
         return
     end
 
-    local job = MOD_Jobs:get(args[2])
+    local job = MOD_Jobs:get(secondArgs)
     if not job then
         player:showSystemNotification(_Literals.ERROR_JOB_NOT_EXIST, NCSEnum.LogType.ERROR)
         return
     end
 
-    target.character:setJob(args[2])
-    if args[3] then 
-        if not MOD_Jobs:get(args[2]).grades[args[3]] then
+    target.character:setJob(secondArgs)
+    if thirdArgs then 
+        if not job.grades[thirdArgs] then
             player:showSystemNotification(_Literals.ERROR_JOB_GRADE_NOT_EXIST, NCSEnum.LogType.ERROR)
             return
         end
 
-        target.character:setJobGrade(args[3])
-        NCS:trace(("Player id ^2%s ^7(^2%s^7) setjob to %s grade %s"):format(target.serverId, target.name, args[2], args[3]), 3)
+        target.character:setJobGrade(thirdArgs)
+        NCS:trace(("Player id ^2%s ^7(^2%s^7) setjob to %s grade %s"):format(target.serverId, target.name, secondArgs, thirdArgs), 3)
     else
-        NCS:trace(("Player id ^2%s ^7(^2%s^7) setjob to %s"):format(target.serverId, target.name, args[2]), 3)
+        NCS:trace(("Player id ^2%s ^7(^2%s^7) setjob to %s"):format(target.serverId, target.name, secondArgs), 3)
     end
 end)
