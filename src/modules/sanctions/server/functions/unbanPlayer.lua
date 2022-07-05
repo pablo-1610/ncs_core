9f3d6f623c9f119fc819d44fdf13818f9a277c5c
MoskalykA <streamarthur462@gmail.com>
2022-07-05 22:19:20 +0200

    refactor: modification of returns

--- src/modules/sanctions/server/functions/unbanPlayer.lua
+++ src/modules/sanctions/server/functions/unbanPlayer.lua
@@ -4,11 +4,13 @@
 ---@public
 function MOD_Sanctions:unbanPlayer(license, ban_id)
     if (not license or not ban_id) then
-        return NCS:die("The license or id of the ban is missing")
+        NCS:die("The license or id of the ban is missing")
+        return
     end
 
     if (not MOD_Sanctions:isPlayerBan(license)) then
-        return NCS:die("This player is not banned")
+        NCS:die("This player is not banned")
+        return
     end
 
     API_Database:query("DELETE FROM ncs_bans WHERE license = ? AND id = ?", {license, ban_id}, function(result)
