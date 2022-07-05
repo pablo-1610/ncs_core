addb3f53cf8911ca212e004c607ae66081a336df
MoskalykA <streamarthur462@gmail.com>
2022-07-05 22:01:35 +0200

    refactor: removal of unnecessary code

--- src/modules/commands/server/revivePlayer.lua
+++ src/modules/commands/server/revivePlayer.lua
@@ -1,22 +1,13 @@
 API_Commands:registerPermissionCommand("revive", "canRevive", function(player, args)
-    local target
-    if args[1] == nil then
-        target = MOD_Players:get(player.serverId) 
-    else 
-        local targetid = tonumber(args[1])
-        target = MOD_Players:get(targetid) 
-    end
+    local target = MOD_Players:get(args[1] == nil and player.serverId or args[1])
     if not target then
         return (player:showSystemNotification(_Literals.TYPE_ERROR, NCSEnum.LogType.ERROR))
-    end
-    if (not (target.character.isDead == 1)) then
-        return (player:showSystemNotification(_Literals.ERROR_PLAYER_NOT_DEAD, NCSEnum.LogType.ERROR))
     end
 
     local playerPed = GetPlayerPed(target.serverId)
     local coords = GetEntityCoords(playerPed)
 	local formattedCoords = {
-		position = vector3( coords[1], coords[2], coords[3] ),
+		position = vector3(coords[1], coords[2], coords[3]),
 		heading = 90.0
 	}
 	while (not formattedCoords) do 
